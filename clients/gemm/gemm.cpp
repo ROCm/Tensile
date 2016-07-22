b690c4cee3c0a6cdd21ddd535e39c784117c26bf
David Tanner <guacamoleo@gmail.com>
2016-07-22 09:05:43 -0500

    validation in gemm

--- clients/gemm/gemm.cpp
+++ clients/gemm/gemm.cpp
@@ -106,12 +106,24 @@ unsigned int addGEMMCombinatorics() {
   //sizes.push_back( {384, 384, 384 });
   //sizes.push_back( {384-1, 384-1, 384 });
   //sizes.push_back( {64, 64, 64});
-  sizes.push_back( { 512  , 512  , 512 });
-  sizes.push_back( { 512  , 512  , 512-1});
-  sizes.push_back( { 512-1, 512-1, 512 });
-  sizes.push_back( { 512-1, 512-1, 512-1});
-  sizes.push_back( { 512+8, 512+8, 512+8});
+  //sizes.push_back( { 512  , 512  , 512 });
+  //sizes.push_back( { 512  , 512  , 512-1});
+  //sizes.push_back( { 512-1, 512-1, 512 });
+  //sizes.push_back( { 512-1, 512-1, 512-1});
+  //sizes.push_back( { 512+8, 512+8, 512+8});
   //sizes.push_back( {4096, 4096, 4096});
+
+#if 1
+  // validation
+  sizes.push_back( { 96*3  , 96*2  , 96*1   });
+  sizes.push_back( { 96*3-1, 96*2  , 96*1   });
+  sizes.push_back( { 96*3  , 96*2-1, 96*1   });
+  sizes.push_back( { 96*3  , 96*2  , 96*1-1 });
+  sizes.push_back( { 96*3-1, 96*2-1, 96*1   });
+  sizes.push_back( { 96*3-1, 96*2  , 96*1-1 });
+  sizes.push_back( { 96*3  , 96*2-1, 96*1-1 });
+  sizes.push_back( { 96*3-1, 96*2-1, 96*1-1 });
+#endif
 
   const size_t numStrides = 1;
   size_t initialStrides[] = { 1, 2 }; // , 64 };
@@ -137,9 +149,9 @@ unsigned int addGEMMCombinatorics() {
   const bool alphas[] = { true, false };
   const size_t numBetas = 1;
   const bool betas[] = { true, false };
-  const size_t numTransA = 1;
+  const size_t numTransA = 2;
   const bool transAs[] = {false, true};
-  const size_t numTransB = 1;
+  const size_t numTransB = 2;
   const bool transBs[] = {true, false};
   const size_t numUseOffsets = 1;
   const bool useOffsets[] = {false, true};
