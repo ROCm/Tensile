42b64fcefbe8b72c6512c407d34e668f26db6764
Paul <pfultz2@yahoo.com>
2016-10-11 19:47:13 -0500

    Add clients to the build

--- cmake/AddCobaltLib.cmake
+++ cmake/AddCobaltLib.cmake
@@ -2,33 +2,49 @@ include(CMakeParseArguments)
 include(CMakeFindDependencyMacro)
 
 function(add_cobalt_lib NAME)
-    set(options OPTIMIZE_ALPHA OPTIMIZE_BETA ENABLE_LOGGER)
-    set(oneValueArgs SOLUTIONS BACKEND)
+    set(options OPTIMIZE_ALPHA OPTIMIZE_BETA ENABLE_LOGGER EXCLUDE_FROM_ALL)
+    set(oneValueArgs SOLUTIONS PROBLEMS BACKEND)
     set(multiValueArgs)
 
     cmake_parse_arguments(PARSE "${options}" "${oneValueArgs}" "${multiValueArgs}" ${ARGN})
 
     set(CobaltLib_DIR_GENERATED "${PROJECT_BINARY_DIR}/cobalt/${NAME}")
 
-    execute_process(
-        COMMAND
-        ${PYTHON_EXECUTABLE} ${Cobalt_DIR}/CobaltGen/CobaltGenBackend.py
-        --backend=${PARSE_BACKEND}
-        --input-path=${PARSE_SOLUTIONS}
-        --output-path=${CobaltLib_DIR_GENERATED}
-        --optimize-alpha=${PARSE_OPTIMIZE_ALPHA}
-        --optimize-beta=${PARSE_OPTIMIZE_BETA}
-    )
+    if(PARSE_SOLUTIONS)
 
-    # Glob CobaltLib source files
-    file(GLOB CobaltLib_SRC
-        ${Cobalt_DIR}/CobaltLib/src/*.cpp
-        ${CobaltLib_DIR_GENERATED}/Kernels/*.cpp
-        ${CobaltLib_DIR_GENERATED}/Solutions/*.cpp
-        ${CobaltLib_DIR_GENERATED}/Other/*.cpp
-    )
+        set(CobaltLib_ENABLE_SOLVER 1)
+        execute_process(
+            COMMAND
+            ${PYTHON_EXECUTABLE} ${Cobalt_DIR}/CobaltGen/CobaltGenBackend.py
+            --backend=${PARSE_BACKEND}
+            --input-path=${PARSE_SOLUTIONS}
+            --output-path=${CobaltLib_DIR_GENERATED}
+            --optimize-alpha=${PARSE_OPTIMIZE_ALPHA}
+            --optimize-beta=${PARSE_OPTIMIZE_BETA}
+        )
+        # Glob CobaltLib source files
+        file(GLOB CobaltLib_SRC
+            ${Cobalt_DIR}/CobaltLib/src/*.cpp
+            ${CobaltLib_DIR_GENERATED}/Kernels/*.cpp
+            ${CobaltLib_DIR_GENERATED}/Solutions/*.cpp
+            ${CobaltLib_DIR_GENERATED}/Other/*.cpp
+        )
 
-    add_library(${NAME} ${CobaltLib_SRC})
+    else()
+        file( WRITE ${CobaltLib_DIR_GENERATED}/Other/SolutionTemplateInstantiations.inl "")
+        # Glob CobaltLib source files
+        file(GLOB CobaltLib_SRC
+            ${Cobalt_DIR}/CobaltLib/src/*.cpp
+        )
+        set(CobaltLib_ENABLE_SOLVER 0)
+
+    endif()
+
+    set(options)
+    if(PARSE_EXCLUDE_FROM_ALL)
+        list(APPEND options EXCLUDE_FROM_ALL)
+    endif()
+    add_library(${NAME} ${options} ${CobaltLib_SRC})
 
     target_include_directories(${NAME}
         PUBLIC  $<BUILD_INTERFACE:${Cobalt_DIR}/CobaltLib/include>
@@ -47,9 +63,18 @@ function(add_cobalt_lib NAME)
     endif()
 
     if( ${PARSE_ENABLE_LOGGER} )
-        target_compile_definitions( ${NAME} PRIVATE -DCobalt_SOLVER_ENABLED=1 -DCobalt_LOGGER_ENABLED=1 )
+        set(CobaltLib_ENABLE_LOGGER 1)
     else()
-        target_compile_definitions( ${NAME} PRIVATE -DCobalt_SOLVER_ENABLED=1 -DCobalt_LOGGER_ENABLED=0 )
+        set(CobaltLib_ENABLE_LOGGER 0)
+    endif()
+
+    target_compile_definitions( ${NAME} PRIVATE 
+        -DCobalt_SOLVER_ENABLED=${CobaltLib_ENABLE_SOLVER} 
+        -DCobalt_LOGGER_ENABLED=${CobaltLib_ENABLE_LOGGER}
+    )
+
+    if(PARSE_PROBLEMS)
+        target_compile_definitions(${NAME} PUBLIC -DCobalt_DIR_PROBLEMS="${PARSE_PROBLEMS}")
     endif()
 
 endfunction()
