448e169291240a71efe3ee9c09c042603edabc6a
David Tanner <guacamoleo@gmail.com>
2016-11-17 12:21:10 -0600

    enabling prints

--- TensileGen/SolutionSelectionWriter.py
+++ TensileGen/SolutionSelectionWriter.py
@@ -34,8 +34,8 @@ class SolutionSelectionWriter:
     self.kernelSet = set()
     self.solutionSet = set()
     #self.scg = SolutionCandidateGenerator.SolutionCandidateGenerator(False, False) # dummy generator for getting indices 0, 1
-    self.printLogic = False
-    self.printStatus = False
+    self.printLogic = True
+    self.printStatus = True
     self.printDebugLib = True
     self.fallbackPSPU1 = None
   
