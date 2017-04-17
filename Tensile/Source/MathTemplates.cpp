b59068c7a99d2173979741b006992f926fb6e01d
David Tanner <guacamoleo@gmail.com>
2017-04-17 15:41:27 -0500

    fixed half-precision validation

--- Tensile/Source/MathTemplates.cpp
+++ Tensile/Source/MathTemplates.cpp
@@ -73,10 +73,10 @@ template<> TensileComplexDouble tensileGetOne<TensileComplexDouble>() {
 * Random Templates
 ******************************************************************************/
 #ifdef Tensile_ENABLE_HALF
-template<> TensileHalf tensileGetRandom<TensileHalf>() { return static_cast<TensileHalf>(rand()%100); }
+template<> TensileHalf tensileGetRandom<TensileHalf>() { return static_cast<TensileHalf>(rand()%10); }
 #endif
 template<> float tensileGetRandom<float>() { return static_cast<float>(rand()%100); }
-template<> double tensileGetRandom<double>() { return static_cast<double>(rand()%100); }
+template<> double tensileGetRandom<double>() { return static_cast<double>(rand()%1000); }
 template<> TensileComplexFloat tensileGetRandom<TensileComplexFloat>() {
   TensileComplexFloat r;
   TENSILEREAL(r) = tensileGetRandom<float>();
