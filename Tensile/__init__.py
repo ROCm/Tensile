0a2878ee0bb74e29f7bb44e2375b7b82094041a9
David Tanner <guacamoleo@gmail.com>
2017-05-10 09:42:42 -0500

    incrementing version after GlobalSplitU feature

--- Tensile/__init__.py
+++ Tensile/__init__.py
@@ -18,4 +18,4 @@
 # IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNE-
 # CTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 ################################################################################
-__version__ = "3.0.0"
+__version__ = "3.1.0"
