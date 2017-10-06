59cc6d9eeb1b1d4dd5b16cc21a35a77f12fa59e3
David Tanner <guacamoleo@gmail.com>
2017-10-06 15:58:03 -0500

    fixing rocm_agent_enumerator for jenkins

--- Tensile/Common.py
+++ Tensile/Common.py
@@ -230,7 +230,7 @@ defaultBenchmarkFinalProblemSizes = [{"Range": [
 # Default Analysis Parameters
 ################################################################################
 defaultAnalysisParameters = {
-    "ScheduleName":       "Default",
+    "ScheduleName":       "Tensile",
     "DeviceNames":  ["Unspecified"],
     #"BranchPenalty":              0, # microseconds / kernel
     "SolutionImportanceMin":      0.01, # = 0.01=1% total time saved by keeping this solution
@@ -315,9 +315,7 @@ def assignGlobalParameters( config ):
 
   # read current gfx version
   if os.name != "nt":
-    #cmdStr = ["/opt/rocm/bin/rocm_agent_enumerator"]
-    cmdStr = ["echo hi"]
-    process = Popen(["rocm_agent_enumerator", "-t", "GPU"], cwd="/opt/rocm/bin/", stdout=PIPE)
+    process = Popen(["/opt/rocm/bin/rocm_agent_enumerator", "-t", "GPU"], stdout=PIPE)
     line = process.stdout.readline()
     while line != "":
       gfxIdx = line.find("gfx")
