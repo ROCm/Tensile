719505325d68680338f269f5a40c6a11fe811319
Ben Sander <ben.sander@amd.com>
2018-02-13 16:55:08 -0600

    Set BufferLoad=True as default

--- Tensile/Common.py
+++ Tensile/Common.py
@@ -277,7 +277,7 @@ defaultBenchmarkCommonParameters = [
     {"LocalWrite2B":              [ True ] },
     {"LocalRead2A":               [ True ] },
     {"LocalRead2B":               [ True ] },
-    {"BufferLoad":                [ False ] },
+    {"BufferLoad":                [ True ] },
     {"GlobalSplitU":              [ 1 ] },
     {"GlobalSplitUSummationAssignmentRoundRobin": [ True ] },
     {"GlobalSplitUWorkGroupMappingRoundRobin":    [ False ] },
