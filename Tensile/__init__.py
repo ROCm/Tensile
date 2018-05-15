8040b1fe1d602b7f52084a328b2ec8fe11f3d36f
amcamd <andrew.chapman@amd.com>
2018-05-15 17:05:50 -0500

    change version for new release

--- Tensile/__init__.py
+++ Tensile/__init__.py
@@ -20,4 +20,4 @@
 ################################################################################
 
 # hardcoded tensile version; also in Tensile/Source/TensileConfigVersion.cmake
-__version__ = "4.1.1"
+__version__ = "4.2.0"
