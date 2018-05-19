555fcc7171a2df3c153ff5d1d4a1fb9e5fb8a269
Ben Sander <ben.sander@amd.com>
2018-05-19 09:56:53 -0500

    Add flat tests

---
+++ Tensile/Tests/nightly/flat/test_flat.py
@@ -0,0 +1,17 @@
+import Tensile.Tensile as Tensile
+
+def test_sgemm_asm_flat_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("flat/test_sgemm_asm_flat_tn.yaml"), tmpdir.strpath])
+
+def test_dgemm_asm_flat(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("flat/test_dgemm_asm_flat.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_flat_tt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("flat/test_sgemm_asm_flat_tt.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_flat_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("flat/test_sgemm_asm_flat_nt.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_flat(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("flat/test_sgemm_asm_flat.yaml"), tmpdir.strpath])
+
