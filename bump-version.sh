e7af0f8dcef98bd0b6383c848f0fac0309beb1d1
amcamd <andrew.chapman@amd.com>
2018-08-09 22:31:44 -0500

    change version for new release

--- bump-version.sh
+++ bump-version.sh
@@ -3,11 +3,11 @@
 # This script needs to be edited to bump version for new release.
 # Version will be bumped in Tensile/__init__.py and in .yaml files
 
-OLD_VERSION="4.2.0"
-NEW_VERSION="4.3.0"
+OLD_VERSION="4.3.0"
+NEW_VERSION="4.4.0"
 
-OLD_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.2.0"
-NEW_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.3.0"
+OLD_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.3.0"
+NEW_MINIMUM_REQUIRED_VERSION="MinimumRequiredVersion: 4.4.0"
 
 sed -i "s/${OLD_VERSION}/${NEW_VERSION}/g" Tensile/__init__.py
 
