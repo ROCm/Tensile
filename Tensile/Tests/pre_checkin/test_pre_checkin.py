02c7538109bfdc9dd8f0b1e6f9316bf2d353b957
Ben Sander <ben.sander@amd.com>
2018-08-31 15:11:25 -0500

    Rename pre-check tests to remove redundant leading test_

--- Tensile/Tests/pre_checkin/test_pre_checkin.py
+++ Tensile/Tests/pre_checkin/test_pre_checkin.py
@@ -1,32 +1,56 @@
 import Tensile.Tensile as Tensile
 
-def test_sgemm_asm(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_sgemm_asm.yaml"), tmpdir.strpath])
+def test_hgemm_hpa_asm_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_asm_tn.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/sgemm_asm_nt.yaml"), tmpdir.strpath])
+
+def test_hgemm_hpa_iu2_asm_tt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_iu2_asm_tt.yaml"), tmpdir.strpath])
+
+def test_hgemm_hpa_iu2_asm_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_iu2_asm_nt.yaml"), tmpdir.strpath])
 
 def test_hgemm_asm_nn(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_asm_nn.yaml"), tmpdir.strpath])
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_asm_nn.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/sgemm_asm_nn.yaml"), tmpdir.strpath])
 
 def test_dgemm_asm(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_dgemm_asm.yaml"), tmpdir.strpath])
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/dgemm_asm.yaml"), tmpdir.strpath])
 
-def test_hgemm_hpa_asm_tn(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_hpa_asm_tn.yaml"), tmpdir.strpath])
-
-def test_hgemm_hpa_asm_nn(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_hpa_asm_nn.yaml"), tmpdir.strpath])
-
-def test_hgemm_asm_tt(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_asm_tt.yaml"), tmpdir.strpath])
-
-def test_hgemm_hpa_asm_tt(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_hpa_asm_tt.yaml"), tmpdir.strpath])
-
-def test_hgemm_asm_nt(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_asm_nt.yaml"), tmpdir.strpath])
+def test_hgemm_hpa_iu2_asm_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_iu2_asm_tn.yaml"), tmpdir.strpath])
 
 def test_hgemm_asm_tn(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_asm_tn.yaml"), tmpdir.strpath])
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_asm_tn.yaml"), tmpdir.strpath])
+
+def test_hgemm_hpa_asm_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_asm_nn.yaml"), tmpdir.strpath])
+
+def test_hgemm_hpa_asm_tt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_asm_tt.yaml"), tmpdir.strpath])
+
+def test_hgemm_asm_tt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_asm_tt.yaml"), tmpdir.strpath])
+
+def test_hgemm_hpa_iu2_asm_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_iu2_asm_nn.yaml"), tmpdir.strpath])
 
 def test_hgemm_hpa_asm_nt(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/test_hgemm_hpa_asm_nt.yaml"), tmpdir.strpath])
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_hpa_asm_nt.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/sgemm_asm_tn.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm_tt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/sgemm_asm_tt.yaml"), tmpdir.strpath])
+
+def test_hgemm_asm_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/hgemm_asm_nt.yaml"), tmpdir.strpath])
+
+def test_sgemm_asm(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("pre_checkin/sgemm_asm.yaml"), tmpdir.strpath])
 
