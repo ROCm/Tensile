ceaf78568b72abdaec7dc1c6f0ae9ae2c579fd56
Jeff Golds <jeffrey.golds@amd.com>
2018-09-14 15:32:24 -0600

    Add check for ASEM a multiple of DepthU.

--- Tensile/SolutionStructs.py
+++ Tensile/SolutionStructs.py
@@ -1400,8 +1400,8 @@ class Solution:
     # For now, LocalDotLayout > 1 only works if the thread tile is a square and VectorWidth is equal to the 
     # thread tile size
     if state["LocalDotLayout"] > 1 and \
-      (state["ThreadTile0"] != state["VectorWidth"] or state["ThreadTile1"] != state["VectorWidth"]):
-      reject(state, "LocalDotLayout > 1 only supports square thread tiles and VectorWidth equal to ThreadTile0/1 size")
+      (state["ThreadTile0"] != state["VectorWidth"] or state["ThreadTile1"] != state["VectorWidth"] or state["AssertSummationElementMultiple"] % state["DepthU"] != 0):
+      reject(state, "LocalDotLayout > 1 only supports square thread tiles and VectorWidth equal to ThreadTile0/1 size and ASEM a multiple of DepthU")
       return
 
     if 0:
