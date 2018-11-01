e0fccb6b3860a3cf1f6e1133eb1802a2dcfcb1f3
William Gilmartin <wgilmart@wgilmart-ubuntu-0.amd.com>
2018-11-01 14:55:33 -0700

    added hpa dot2 nightly tests

---
+++ Tensile/Tests/nightly/dot2/test_dot2.py
@@ -0,0 +1,8 @@
+import Tensile.Tensile as Tensile
+
+def test_hgemm_hpa_dot2_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/dot2/hgemm_hpa_dot2_nn.yaml"), tmpdir.strpath])
+
+def test_hgemm_hpa_dot2_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/dot2/hgemm_hpa_dot2_tn.yaml"), tmpdir.strpath])
+
