de8e6b5f629a40f8f1ed1f2e9ce435147929b7af
Ben Sander <ben.sander@amd.com>
2018-12-02 07:05:12 -0600

    Add tests for pack_tensor_dims

---
+++ Tensile/Tests/nightly/pack_tensor_dims/test_pack_tensor_dims.py
@@ -0,0 +1,20 @@
+import Tensile.Tensile as Tensile
+
+def test_strideb0_pack_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/strideb0_pack_tn.yaml"), tmpdir.strpath])
+
+def test_strideb0_pack_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/strideb0_pack_nn.yaml"), tmpdir.strpath])
+
+def test_simple_strideb0_pack(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/simple_strideb0_pack.yaml"), tmpdir.strpath])
+
+def test_stridea0_pack_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/stridea0_pack_nt.yaml"), tmpdir.strpath])
+
+def test_simple_stridea0_pack(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/simple_stridea0_pack.yaml"), tmpdir.strpath])
+
+def test_strideb0_pack_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/strideb0_pack_nt.yaml"), tmpdir.strpath])
+
