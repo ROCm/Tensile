5ec2faaa65df61937726cb966da7390763a4b3a7
Ben Sander <ben.sander@amd.com>
2018-12-09 10:42:25 -0500

    Add more GSU/beta tests

--- Tensile/Tests/nightly/global_split_u/test_global_split_u.py
+++ Tensile/Tests/nightly/global_split_u/test_global_split_u.py
@@ -3,6 +3,12 @@ import Tensile.Tensile as Tensile
 def test_hgemm_gsu(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/global_split_u/hgemm_gsu.yaml"), tmpdir.strpath])
 
-def test_sgemm_gsu(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("nightly/global_split_u/sgemm_gsu.yaml"), tmpdir.strpath])
+def test_sgemm_gsu_beta2(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/global_split_u/sgemm_gsu_beta2.yaml"), tmpdir.strpath])
+
+def test_sgemm_gsu_beta1(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/global_split_u/sgemm_gsu_beta1.yaml"), tmpdir.strpath])
+
+def test_sgemm_gsu_beta0(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/global_split_u/sgemm_gsu_beta0.yaml"), tmpdir.strpath])
 
