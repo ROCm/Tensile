6e50800e08e5a0eb6c42a1f4a33d2ca9f25731b9
Ben Sander <ben.sander@amd.com>
2018-12-15 12:27:16 -0500

    Shorten nightly tests

---
+++ Tensile/Tests/weekly/classic_source/test_classic_source.py
@@ -0,0 +1,8 @@
+import Tensile.Tensile as Tensile
+
+def test_hgemm_vectors(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("weekly/classic_source/test_hgemm_vectors.yaml"), tmpdir.strpath])
+
+def test_sgemm_vectors(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("weekly/classic_source/test_sgemm_vectors.yaml"), tmpdir.strpath])
+
