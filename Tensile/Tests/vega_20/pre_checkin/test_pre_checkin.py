e55e41a2e5ed75f1c3b185c7277554a6aa389244
amcamd <andrew.chapman@amd.com>
2019-01-07 18:31:35 -0500

    assembly igemm pre_checkin, nighly/gsu, nightly/lsu tests

---
+++ Tensile/Tests/vega_20/pre_checkin/test_pre_checkin.py
@@ -0,0 +1,13 @@
+import Tensile.Tensile as Tensile
+
+def test_igemm_asm_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/pre_checkin/igemm_asm_nn.yaml"), tmpdir.strpath])
+
+def test_igemm_asm_nt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/pre_checkin/igemm_asm_nt.yaml"), tmpdir.strpath])
+
+def test_igemm_asm_tt(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/pre_checkin/igemm_asm_tt.yaml"), tmpdir.strpath])
+
+def test_igemm_asm_tn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/pre_checkin/igemm_asm_tn.yaml"), tmpdir.strpath])
