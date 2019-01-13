4598cb84430e45b227e84f01e6a7d87e32004618
Ben Sander <ben.sander@amd.com>
2019-01-13 14:15:10 -0500

    fix staggeru disable - was always enabled if BufferLoad

--- Tensile/KernelWriter.py
+++ Tensile/KernelWriter.py
@@ -1074,7 +1074,7 @@ class KernelWriter:
   @abc.abstractmethod
   def initKernel(self, kernel, tensorParametersA, tensorParametersB ):
 
-    self.staggerU = kernel["StaggerU"] and kernel["KernelLanguage"]=="Source" or kernel["BufferLoad"]
+    self.staggerU = kernel["StaggerU"] and (kernel["KernelLanguage"]=="Source" or kernel["BufferLoad"])
 
     # Only assembly supports scheduling
     self.canSchedule = kernel["KernelLanguage"] == "Assembly"
