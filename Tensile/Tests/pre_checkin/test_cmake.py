94e3f7a6347fe62b7fd0546b69c21e52402662e8
Paul <pfultz2@yahoo.com>
2019-04-10 16:45:33 -0500

    Set compiler to hcc

--- Tensile/Tests/pre_checkin/test_cmake.py
+++ Tensile/Tests/pre_checkin/test_cmake.py
@@ -7,11 +7,24 @@ def get_root(p):
         r = os.path.split(r[0])
     return r[0]
 
+def which(p, paths=None):
+    exes = [p+x for x in ['', '.exe', '.bat']]
+    for dirname in list(paths or [])+os.environ['PATH'].split(os.pathsep):
+        for exe in exes:
+            candidate = os.path.join(os.path.expanduser(dirname), exe)
+            if os.path.isfile(candidate):
+                return candidate
+    return None
+
 __test_dir__ = os.path.dirname(os.path.realpath(__file__))
 
 __tensile_dir__ = get_root(Tensile.__file__)
 
 __cmake_dir__ = os.path.join(__test_dir__, 'cmake')
 
+__compiler__ = which('hcc', paths=['/opt/rocm/bin'])
+
 def test_cmake(tmpdir):
-    subprocess.check_call(['cmake', '-DCMAKE_PREFIX_PATH='+__tensile_dir__, __cmake_dir__], cwd=tmpdir.strpath)
+    env = os.environ
+    env['CXX'] = __compiler__
+    subprocess.check_call(['cmake', '-DCMAKE_PREFIX_PATH='+__tensile_dir__, __cmake_dir__], cwd=tmpdir.strpath, env=env)
