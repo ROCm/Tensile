44f3e2f70f6e6f0c8dc11bdaaab13fe1d71aa2a8
Akila Premachandra <apremach@cgy-eevee.amd.com>
2019-04-09 18:59:47 -0600

    Revert "Issue with Library Logic (stage 3) and ClientWriter.py has been resolved. Reformatting has been finished as well."
    This reverts commit 1adeefc0fe69df787b642daa2ef518857c334998 to remove some reformatting.

--- Tensile/ClientWriter.py
+++ Tensile/ClientWriter.py
@@ -1,5 +1,5 @@
 ################################################################################
-# Copyright (C) 2016-2019 Advanced Micro Devices, Inc. All rights reserved.
+# Copyright (C) 2016 Advanced Micro Devices, Inc. All rights reserved.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
@@ -278,6 +278,8 @@ def writeClientParameters(forBenchmark, solutions, problemSizes, stepName, \
         solutionWriter = SolutionWriter( \
             solutionMinNaming, solutionSerialNaming, \
             kernelMinNaming, kernelSerialNaming)
+
+    if forBenchmark:
         if globalParameters["MergeFiles"]:
             h += "#include \"Solutions.h\"\n"
         else:
@@ -374,10 +376,10 @@ def writeClientParameters(forBenchmark, solutions, problemSizes, stepName, \
         dataTypes = sorted(dataTypes)
         for dataType in dataTypes:
             problemTypesForDataType[dataType] = \
-                sorted(problemTypesForDataType[dataType],key=str)
+                sorted(problemTypesForDataType[dataType])
             for problemType in problemTypesForDataType[dataType]:
                 schedulesForProblemType[problemType] = \
-                    sorted(schedulesForProblemType[problemType],key=str)
+                    sorted(schedulesForProblemType[problemType])
 
         # assign info
         functionIdxSerial = 0
@@ -939,7 +941,8 @@ def writeClientParameters(forBenchmark, solutions, problemSizes, stepName, \
                 functionsForDataType = []
                 for problemType in problemTypesForDataType[dataType]:
                     for scheduleName in schedulesForProblemType[problemType]:
-                        functionsForDataType.append([scheduleName, problemType])
+                        functionsForDataType.list(
+                            append([scheduleName, problemType]))
                 h += "template<>\n"
                 h += "inline %s generatedCallTo_%s<%s>(\n" \
                     % (returnName, functionName, typeName)
