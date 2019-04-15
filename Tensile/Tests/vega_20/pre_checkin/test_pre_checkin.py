d7d3de8a517c81b785396b035eec217fbde038fb
mahmoodw <wasiq.mahmood@amd.com>
2019-04-15 23:53:04 +0000

    Adding pre check in

--- Tensile/Tests/vega_20/pre_checkin/test_pre_checkin.py
+++ Tensile/Tests/vega_20/pre_checkin/test_pre_checkin.py
@@ -11,3 +11,6 @@ def test_igemm_asm_tt(tmpdir):
 
 def test_igemm_asm_tn(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("vega_20/pre_checkin/igemm_asm_tn.yaml"), tmpdir.strpath])
+
+def test_gemm_ldc_equals_ldd(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/pre_checkin/gemm_ldc_equals_ldd.yaml"), tmpdir.strpath])
