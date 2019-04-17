170c2271156e1baf860d5c6c70cb2b2b67e308d5
mahmoodw <wmahmood@cgy-pikachu.amd.com>
2019-04-17 17:51:58 -0600

    Splitting up nightlys

--- Tensile/Tests/vega_20/nightly/ldc_equals_ldd/test_ldc_equals_ldd.py
+++ Tensile/Tests/vega_20/nightly/ldc_equals_ldd/test_ldc_equals_ldd.py
@@ -1,6 +1,7 @@
 import Tensile.Tensile as Tensile
 
 def test_gemm_ldc_equals_ldd(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("vega_20/nightly/ldc_equals_ldd/gemm_ldc_equals_ldd.yaml"), tmpdir.strpath])
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/nightly/ldc_equals_ldd/gemm_ldc_equals_ldd_nt.yaml"), tmpdir.strpath])
 
-
+def test_gemm_ldc_equals_ldd(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("vega_20/nightly/ldc_equals_ldd/gemm_ldc_equals_ldd_nn.yaml"), tmpdir.strpath])
