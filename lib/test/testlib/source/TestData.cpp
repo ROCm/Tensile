2c7e51adbc3cd1c6f7a145c16cee45ed4252a6d3
Scott Quiring <scott.quiring@amd.com>
2019-05-30 11:10:40 -0600

    Starting to move test utilities into a library.

---
+++ lib/test/testlib/source/TestData.cpp
@@ -0,0 +1,142 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright (c) 2019 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include "TestData.hpp"
+
+#include <glob.h>
+#include <unistd.h>
+
+#include <boost/version.hpp>
+
+#if BOOST_VERSION >= 106100
+#include <boost/dll/runtime_symbol_info.hpp>
+#else
+#define TEST_DATA_USE_PROC_EXE
+#endif
+
+#include <gtest/gtest.h>
+
+#include <Tensile/Utils.hpp>
+
+TestData::operator bool() const
+{
+    return boost::filesystem::is_directory(dataDir());
+}
+
+TestData TestData::Invalid()
+{
+    return TestData(invalid_data());
+}
+
+TestData TestData::Env(std::string const& varName)
+{
+    char * var = getenv(varName.c_str());
+
+    if(var == nullptr)
+        return Invalid();
+
+    return TestData(var);
+}
+
+boost::filesystem::path TestData::dataDir() const
+{
+    return m_dataDir;
+}
+
+boost::filesystem::path TestData::file(std::string const& filename) const
+{
+    return dataDir() / filename;
+}
+
+std::vector<boost::filesystem::path> TestData::glob(std::string const& pattern) const
+{
+    std::string wholePattern = (dataDir() / pattern).native();
+
+    glob_t result;
+    result.gl_pathc = 0;
+    result.gl_pathv = nullptr;
+    result.gl_offs = 0;
+
+    int err = ::glob(wholePattern.c_str(), 0, nullptr, &result);
+
+    // This way globfree will be called regardless of if an exception is thrown.
+    std::shared_ptr<glob_t> guard(&result, globfree);
+
+    if(err == GLOB_NOSPACE || err == GLOB_ABORTED)
+        throw std::runtime_error(Tensile::concatenate("Glob ", wholePattern, " failed."));
+
+    std::vector<boost::filesystem::path> rv(result.gl_pathc);
+
+    for(size_t i = 0; i < result.gl_pathc; i++)
+        rv[i] = result.gl_pathv[i];
+
+    return rv;
+}
+
+boost::filesystem::path TestData::ProgramLocation()
+{
+#ifdef TEST_DATA_USE_PROC_EXE
+    return boost::filesystem::read_symlink("/proc/self/exe");
+#else
+    return boost::dll::program_location();
+#endif
+
+}
+
+TestData::TestData()
+    : m_dataDir(ProgramLocation().parent_path() / "data")
+{
+}
+
+TestData::TestData(std::string const& dataDir)
+    : m_dataDir(dataDir)
+{
+}
+
+TestData::TestData(invalid_data)
+{
+}
+
+TEST(TestData, Simple)
+{
+    auto data = TestData::Instance();
+
+    EXPECT_TRUE(static_cast<bool>(data));
+
+    auto is_regular_file = static_cast<bool (*)(boost::filesystem::path const&)>(boost::filesystem::is_regular_file);
+
+    EXPECT_PRED1(is_regular_file, data.file("KernelsLite.yaml"));
+    EXPECT_FALSE(is_regular_file( data.file("fjdlksljfjldskj")));
+
+    auto files = data.glob("*.yaml");
+    EXPECT_EQ(files.size(), 3);
+    for(auto file: files)
+        EXPECT_PRED1(is_regular_file, file);
+
+    if(TestData::Env("TENSILE_NEVER_SET_THIS_AKDJFLKDSJ"))
+        FAIL() << "TestData object constructed with unset environment variable should convert to false!";
+}
+
