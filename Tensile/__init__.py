a3caeaace166a6ff11fea4eb34d312fa7e16191b
amcamd <andrew.chapman@amd.com>
2019-06-25 16:44:46 -0500

    change version for periodic merge

--- Tensile/__init__.py
+++ Tensile/__init__.py
@@ -20,4 +20,4 @@
 ################################################################################
 
 # hardcoded tensile version; also in Tensile/Source/TensileConfigVersion.cmake
-__version__ = "4.9.10"
+__version__ = "4.9.11"
