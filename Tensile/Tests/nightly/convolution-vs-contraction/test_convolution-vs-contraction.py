ad586c9f8711ab403792ba12a996f7c9c4145503
Ben Sander <ben.sander@amd.com>
2019-09-30 14:37:03 -0400

    conv - update tests

--- Tensile/Tests/nightly/convolution-vs-contraction/test_convolution-vs-contraction.py
+++ Tensile/Tests/nightly/convolution-vs-contraction/test_convolution-vs-contraction.py
@@ -6,8 +6,8 @@ def test_simple_forward_nchw_ckyx_3xN(tmpdir):
 def test_simple_forward_cnhw_defaults(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/simple_forward_cnhw_defaults.yaml"), tmpdir.strpath])
 
-def test_simple_nchw_example(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/simple_nchw_example.yaml"), tmpdir.strpath])
+def test_example_0(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/example_0.yaml"), tmpdir.strpath])
 
 def test_forward_nchw_defaults(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/forward_nchw_defaults.yaml"), tmpdir.strpath])
@@ -17,6 +17,9 @@ def test_simple_forward_nchw_ckyx_3x4(tmpdir):
 
 def test_simple_forward_nchw_3x1(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/simple_forward_nchw_3x1.yaml"), tmpdir.strpath])
+
+def test_simple_strides(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/simple_strides.yaml"), tmpdir.strpath])
 
 def test_forward_nchw_kcyx_cnhw(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/convolution-vs-contraction/forward_nchw_kcyx_cnhw.yaml"), tmpdir.strpath])
