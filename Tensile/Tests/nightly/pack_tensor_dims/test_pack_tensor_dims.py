e050daf0e2f3e555e10f3e3b861b26c495d4f1e2
Ben Sander <ben.sander@amd.com>
2019-10-01 12:20:34 -0400

    Restore pack_tensor_dims tests from banishment
    -Document bug with multi free and tile idx in source generator

--- Tensile/Tests/nightly/pack_tensor_dims/test_pack_tensor_dims.py
+++ Tensile/Tests/nightly/pack_tensor_dims/test_pack_tensor_dims.py
@@ -1,8 +1,5 @@
 import Tensile.Tensile as Tensile
 
-def test_packed_perf_nn(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/packed_perf_nn.yaml"), tmpdir.strpath])
-
 def test_strideb0_pack_tn(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/strideb0_pack_tn.yaml"), tmpdir.strpath])
 
@@ -11,6 +8,12 @@ def test_simple_stridea0_pack(tmpdir):
 
 def test_simple_strideb0_pack(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/simple_strideb0_pack.yaml"), tmpdir.strpath])
+
+def test_multi_free_batch(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/multi_free_batch.yaml"), tmpdir.strpath])
+
+def test_packed_perf_nn(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/packed_perf_nn.yaml"), tmpdir.strpath])
 
 def test_strideb0_pack_nt(tmpdir):
  Tensile.Tensile([Tensile.TensileTestPath("nightly/pack_tensor_dims/strideb0_pack_nt.yaml"), tmpdir.strpath])
