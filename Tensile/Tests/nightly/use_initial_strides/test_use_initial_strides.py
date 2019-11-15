7ba32c6f8000e2f48a53f027847a4f6ae7e57adc
Ben Sander <ben.sander@amd.com>
2019-11-15 13:38:05 -0500

    Add use_init_strides tests

--- Tensile/Tests/nightly/use_initial_strides/test_use_initial_strides.py
+++ Tensile/Tests/nightly/use_initial_strides/test_use_initial_strides.py
@@ -1,5 +1,17 @@
 import Tensile.Tensile as Tensile
 
+def test_2(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/use_initial_strides/test_2.yaml"), tmpdir.strpath])
+
+def test_1(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/use_initial_strides/test_1.yaml"), tmpdir.strpath])
+
+def test_strides(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/use_initial_strides/test_strides.yaml"), tmpdir.strpath])
+
+def test_strides1(tmpdir):
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/use_initial_strides/test_strides1.yaml"), tmpdir.strpath])
+
 def test_simple_use_initial_strides_1(tmpdir):
- Tensile.Tensile([Tensile.TensileTestPath("use_initial_strides/simple_use_initial_strides_1.yaml"), tmpdir.strpath])
+ Tensile.Tensile([Tensile.TensileTestPath("nightly/use_initial_strides/simple_use_initial_strides_1.yaml"), tmpdir.strpath])
 
