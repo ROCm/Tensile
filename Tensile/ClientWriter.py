b2f3c6b8ce709a1fc79d03a5ed9522606b27facf
Akila Premachandra <akila.premachandra@amd.com>
2019-12-06 18:06:43 -0700

    More edits

--- Tensile/ClientWriter.py
+++ Tensile/ClientWriter.py
@@ -174,6 +174,7 @@ def writeRunScript(path, libraryLogicPath, forBenchmark, enableTileSelection):
       runScriptFile.write(" -DTensile_ROOT=%s" % globalParameters["ScriptPath"] )
       runScriptFile.write(" -DTensile_CLIENT_BENCHMARK=OFF")
       runScriptFile.write(" -DTensile_LOGIC_PATH=%s" % libraryLogicPath)
+      runScriptFile.write(" -DTensile_ARCHITECTURE=%s" % globalParameters["Architecture"])
       runScriptFile.write(" -DTensile_LIBRARY_PRINT_DEBUG=%s" \
           % ("ON" if globalParameters["LibraryPrintDebug"] else "OFF"))
       runScriptFile.write(" -DTensile_SHORT_FILE_NAMES=%s" \
