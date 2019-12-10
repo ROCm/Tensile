73ba3747ce559684028821986b5771cab4f653ef
Scott Quiring <scott.quiring@amd.com>
2019-12-09 22:01:24 -0700

    Fixing gfx1010 arch.

--- Tensile/SolutionLibrary.py
+++ Tensile/SolutionLibrary.py
@@ -23,6 +23,7 @@ import itertools
 
 from . import Properties
 from . import Hardware
+from . import Common
 from . import Contractions
 from .SolutionStructs import Solution as OriginalSolution
 from .Utils import state
@@ -242,7 +243,7 @@ class MasterSolutionLibrary:
                 if devicePart == 'fallback':
                     pred = Hardware.HardwarePredicate("TruePred")
                 else:
-                    isa = tuple(map(int,devicePart[3:6]))
+                    isa = Common.gfxArch(devicePart)
                     pred = Hardware.HardwarePredicate.FromISA(isa)
 
                 newLib.rows.append({'predicate': pred, 'library': library})
