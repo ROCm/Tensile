fa02cd2f5030774ef1607a4557c43c6bf5cb95f6
Ben Sander <ben.sander@amd.com>
2019-12-16 17:24:53 -0500

    Run multiple solutions for Contraction tests
    - track desired solution in Runner class
    - Run_Contraction tests can run multiple solutions; others just 1
    - move src1, asm3 tests to Solution class
    - add more cases (VectorWidth==4) to asm3

--- Tensile/Tests/extended/convolution_config/test_yaml.py
+++ Tensile/Tests/extended/convolution_config/test_yaml.py
@@ -16,4 +16,4 @@ def test_yaml(run_convolution_level):
     assert(z['SetConstStrideB']==[[2,0]])
     assert(z['UseInitialStridesAB']==0)
 
-    run_convolution_level(conv, z)
+    run_convolution_level.func(conv, z, run_convolution_level.solution)
