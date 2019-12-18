6c96947b062f31a82f40a4da42e3ebfa4a3dc4c7
Alex Brown <alex.brown@amd.com>
2019-12-18 23:16:35 +0000

    Fix new client merge

--- Tensile/Source/client/source/ReferenceValidator.cpp
+++ Tensile/Source/client/source/ReferenceValidator.cpp
@@ -252,7 +252,7 @@ namespace Tensile
             if(!m_enabled)
                 return rv;
 
-            if(m_printTensorA || m_printTensorB || m_printTensorC || m_printTensorD || m_printTensorRef)
+            if(m_printTensorA || m_printTensorB || m_printTensorC || m_printTensorD)
                 printTensorsTyped(reference, result);
 
             if(m_elementsToValidate != 0)
