5c4ee80bd455bb2233b308eec6c2b95b72ac82cf
Ben Sander <ben.sander@amd.com>
2020-01-03 12:56:35 -0500

    Update tests with dict AssertStride* syntax

--- Tensile/Tests/extended/convolution_config/test_forward_cnhw.py
+++ Tensile/Tests/extended/convolution_config/test_forward_cnhw.py
@@ -12,8 +12,8 @@ def test_cnhw_defaults(run_convolution_level):
     assert(z['IndexAssignmentsA']==[0, 1, 3])
     assert(z['IndexAssignmentsB']==[3, 2, 1])
     assert(not z['UseInitialStridesAB'])
-    assert(conv.solutionParms["AssertStrideAEqual"] == "0:1")
-    assert(conv.solutionParms["AssertStrideBEqual"] == "0:1,2:0")
+    assert(conv.solutionParms["AssertStrideAEqual"] == {0:1})
+    assert(conv.solutionParms["AssertStrideBEqual"] == {0:1,2:0})
     assert(conv.solutionParms["AssertSizeEqual"] == {})
 
     run_convolution_level.func(conv, z, run_convolution_level.solution)
--- Tensile/Tests/extended/convolution_config/test_simple.py
+++ Tensile/Tests/extended/convolution_config/test_simple.py
@@ -14,7 +14,7 @@ def test_simple(run_convolution_level):
     assert(z['NumIndicesC']==3)
     assert(z['IndexAssignmentsA']==[0, 3, 2])
     assert(z['IndexAssignmentsB']==[3, 1, 2])
-    assert(conv.solutionParms["AssertStrideAEqual"] == "0:1")
-    assert(conv.solutionParms["AssertStrideBEqual"] == "0:1,2:0")
+    assert(conv.solutionParms["AssertStrideAEqual"] == {0:1})
+    assert(conv.solutionParms["AssertStrideBEqual"] == {0:1,2:0})
 
     run_convolution_level.func(conv, z, run_convolution_level.solution)
