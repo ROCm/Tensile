dfc7e9e37a0c92e79b93abff3cb7c56ff9140a94
Eiden Yoshida <eiden.yoshida@amd.com>
2020-01-06 11:41:57 -0700

    Split Jenkinsfile into Common, Debug build, PreCheckin Tests, and Extended Tests

---
+++ .jenkins/Common.groovy
@@ -0,0 +1,76 @@
+// This file is for internal AMD use.
+// If you are interested in running your own Jenkins, please raise a github issue for assistance.
+
+def runCompileCommand(platform, project)
+{
+    try
+    {
+        project.paths.construct_build_prefix()
+
+        def command = """#!/usr/bin/env bash
+                set -ex
+
+                hostname
+
+                export PATH=/opt/rocm/bin:$PATH
+                cd ${project.paths.project_build_prefix}
+
+                mkdir build
+                pushd build
+                ${project.paths.build_command}
+                make -j
+
+                popd
+                tox --version
+                tox -v --workdir /tmp/.tensile-tox -e lint
+                """
+
+        platform.runCommand(this, command)
+
+        publishHTML([allowMissing: false,
+                    alwaysLinkToLastBuild: false,
+                    keepAll: false,
+                    reportDir: "${project.paths.project_build_prefix}/docs/html",
+                    reportFiles: 'index.html',
+                    reportName: 'Documentation',
+                    reportTitles: 'Documentation'])
+
+        
+    }
+    finally
+    {
+    }
+}
+
+def runTestCommand (platform, project, test_marks)
+{
+    def test_dir =  "Tensile/Tests"
+
+    try
+    {
+        def command = """#!/usr/bin/env bash
+                set -ex
+
+                hostname
+
+                export PATH=/opt/rocm/bin:$PATH
+                cd ${project.paths.project_build_prefix}
+
+                pushd build
+                ./TensileTests --gtest_output=xml:host_test_output.xml --gtest_color=yes
+
+                popd
+                tox --version
+                tox -v --workdir /tmp/.tensile-tox -e py35 -- ${test_dir} -m "${test_marks}"
+                date
+            """
+        platform.runCommand(this, command)
+    }
+    finally
+    {
+        junit "${project.paths.project_build_prefix}/build/host_test_output.xml"
+        junit "${project.paths.project_build_prefix}/*_tests.xml"
+    }
+}
+
+return this
