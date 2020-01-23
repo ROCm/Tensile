5ccdcd1be67d8bdaf45ba8dff177c9c7766d5269
Ben Sander <ben.sander@amd.com>
2020-01-22 22:59:58 -0500

    Remove useinitialstrides market from beta kernels

--- Tensile/KernelWriter.py
+++ Tensile/KernelWriter.py
@@ -2736,8 +2736,6 @@ for codeObjectFileName in codeObjectFileNames:
     name += "_"
     name += kernel["ProblemType"]["DestDataType"].toChar()
     if kernel["ProblemType"]["UseBeta"]: name += "B"
-    if kernel["ProblemType"]["UseInitialStridesAB"]: name += "I"
-    if kernel["ProblemType"]["UseInitialStridesCD"]: name += "Ic"
 
     return name
 
