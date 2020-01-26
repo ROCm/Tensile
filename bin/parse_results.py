081649290c72162cf4caf1e8ef245e67f8d6d8dc
Ben Sander <ben.sander@amd.com>
2020-01-26 09:36:45 -0500

    Update parse_results
    - standardize fieldsToPrint printing, add header
    - track best perf for any solution

--- bin/parse_results.py
+++ bin/parse_results.py
@@ -2,10 +2,22 @@
 
 import sys,fileinput, csv
 
+class Solution:
+    def __init__(self):
+        self.problemResults = []
+        self.maxPerf = None
+
+    def addRow(self,row):
+        self.problemResults.append(row)
+        if self.maxPerf==None or float(row["gflops"]) > float(self.maxPerf["gflops"]):
+            self.maxPerf = row
+
+
 class Reader:
 
-    def formatCol(self, row, colName):
-        return ('{0: <%d}'%self.maxLen[colName]).format(row[colName])
+    def formatCol(self, row, colName, val=None):
+        return ('{0: <%d}'%self.maxLen[colName]).format(val if val else row[colName])
+
 
     def readFile(self):
         db = 0
@@ -22,19 +34,20 @@ class Reader:
 
         dictReader = csv.DictReader(csv_table, csv_field_names, skipinitialspace=True)
 
-        fieldsToKeep= ('problem-sizes', 'solution', 'time-us', 'gflops')
+        fieldsToPrint= ('problem-sizes', 'solution', 'time-us', 'gflops', "alu-us","mem-read-us","mem-write-us", 'total-granularity')
+        derivedFields = ['best-any']
 
         self.maxLen={}
-        for k in fieldsToKeep:
-            self.maxLen[k] = 0
+        for k in fieldsToPrint:
+            self.maxLen[k] = len(k) # length of name
 
         self.dataOut=[]
+        solutions = {} # map from solution to Solution class
         for row in dictReader:
-            if db:
-                print("R=", (row["solution"], row["time-us"], row["gflops"]))
+            solutions.setdefault(row["solution"],Solution()).addRow(row)
 
             keep = {}
-            for k in fieldsToKeep:
+            for k in fieldsToPrint:
                 keep[k] = row[k]
                 self.maxLen[k] = max(len(row[k]), self.maxLen[k])
 
@@ -43,9 +56,20 @@ class Reader:
         # sort by gflops
         self.dataOut.sort(key = lambda row: int(row["gflops"]), reverse=True)
 
-        for row in self.dataOut:
-            print (self.formatCol(row,'problem-sizes'), self.formatCol(row, 'solution'), " %6.1f %6d" %(float(row['time-us']), int(row['gflops'])))
+        # print header:
+        for field in fieldsToPrint:
+            print (self.formatCol(row,field,val=field), end=" ")
+        for field in derivedFields:
+            print (field,end=" ")
+        print()
 
+        for row in self.dataOut:
+            for field in fieldsToPrint:
+                print (self.formatCol(row,field), end=" ")
+            print (solutions[row["solution"]].maxPerf["gflops"], end="") # max perf from any solution
+            print()
+
+            #print (self.formatCol(row,'problem-sizes'), self.formatCol(row, 'solution'), " %6.1f %6d" %(float(row['time-us']), int(row['gflops'])))
 
 
 r = Reader()
