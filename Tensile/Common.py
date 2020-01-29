e411d06d81aa75a55138e842cd443a430ebdb561
Ben Sander <ben.sander@amd.com>
2020-01-28 20:38:47 -0500

    Update PSD GSU tests to enable Assembly

--- Tensile/Common.py
+++ Tensile/Common.py
@@ -162,7 +162,7 @@ globalParameters["ShortNames"] = False            # on windows kernel names can 
 globalParameters["MergeFiles"] = True             # F=store every solution and kernel in separate file; T=store all solutions in single file
 globalParameters["SupportedISA"] = [(8,0,3), (9,0,0), (9,0,6), (9,0,8), (10,1,0)]             # assembly kernels writer supports these architectures
 globalParameters["ClientBuildPath"] = "0_Build"                   # subdirectory for host code build directory.
-globalParameters["NewClient"] = 1                                 # 1=Run old+new client, 2=run new client only (All In)
+globalParameters["NewClient"] = 2                                 # 1=Run old+new client, 2=run new client only (All In)
 globalParameters["BenchmarkProblemsPath"] = "1_BenchmarkProblems" # subdirectory for benchmarking phases
 globalParameters["BenchmarkDataPath"] = "2_BenchmarkData"         # subdirectory for storing final benchmarking data
 globalParameters["LibraryLogicPath"] = "3_LibraryLogic"           # subdirectory for library logic produced by analysis
@@ -828,7 +828,7 @@ defaultBenchmarkCommonParameters = [
     {"UnrollIncIsDepthU":         [ 0 ] },
     {"PackGranularity":           [ 2 ] },
     {"FractionalLoad":            [ 0 ] },
-    {"Use64bShadowLimit":         [ 1 ] },
+    {"Use64bShadowLimit":         [ 0 ] },
     {"VectorAtomicWidth":         [ -1 ] },
 
     {"NumLoadsCoalescedA":        [ 1 ] },
