014d792124ead52406eaa6223c41e4a0888fa8f2
Saad Rahim <saad.rahim@amd.com>
2020-02-05 10:16:41 -0800

    Refactoring files

--- .jenkins/debug.groovy
+++ .jenkins/debug.groovy
@@ -12,12 +12,15 @@ import java.nio.file.Path
 properties(auxiliary.setProperties())
 
 
-tensileCI:
+def runCI =
 {
+    nodeDetails, jobName->
+
     def tensile = new rocProject('Tensile', 'Debug')
     tensile.paths.build_command = 'cmake -D CMAKE_BUILD_TYPE=Debug -D CMAKE_CXX_COMPILER=hcc -DTensile_ROOT=$(pwd)/../Tensile ../HostLibraryTests'
+
     // Define test architectures, optional rocm version argument is available
-    def nodes = new dockerNodes(['ubuntu'], tensile)
+    def nodes = new dockerNodes(nodeDetails, jobName, prj)
 
     boolean formatCheck = false
 
@@ -35,4 +38,44 @@ tensileCI:
 
     buildProject(tensile, formatCheck, nodes.dockerArray, compileCommand, null, null)
 
+}
+
+
+ci: {
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 0')])],
+                        "compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])],
+                        "rocm-docker":[]]
+    propertyList = auxiliary.appendPropertyList(propertyList)
+
+    Set standardJobNameSet = ["compute-rocm-dkms-no-npi", "compute-rocm-dkms-no-npi-hipclang", "rocm-docker"]
+
+    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu16:['any']]),
+                       "compute-rocm-dkms-no-npi-hipclang":([ubuntu16:['any']]),
+                       "rocm-docker":([ubuntu16:['any']])]
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
+
+    propertyList.each
+    {
+        jobName, property->
+        if (urlJobName == jobName)
+            properties(auxiliary.addCommonProperties(property))
+    }
+
+    Set seenJobNames = []
+    jobNameList.each
+    {
+        jobName, nodeDetails->
+        seenJobNames.add(jobName)
+        if (urlJobName == jobName)
+            runCI(nodeDetails, jobName)
+    }
+
+    // For url job names that are outside of the standardJobNameSet i.e. compute-rocm-dkms-no-npi-1901
+    if(!seenJobNames.contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        runCI([ubuntu16:['any']], urlJobName)
+    }
 }
--- .jenkins/prechecking.groovy
+++ .jenkins/prechecking.groovy
@@ -1,6 +1,6 @@
 #!/usr/bin/env groovy
 // This shared library is available at https://github.com/ROCmSoftwarePlatform/rocJENKINS/
-@Library('rocJenkins') _
+@Library('rocJenkins@pong') _
 
 // This is file for internal AMD use.
 // If you are interested in running your own Jenkins, please raise a github issue for assistance.
@@ -12,12 +12,15 @@ import java.nio.file.Path
 properties(auxiliary.setProperties())
 
 
-tensileCI:
+def runCI =
 {
+    nodeDetails, jobName->
+
     def tensile = new rocProject('Tensile', 'PreCheckin')
     tensile.paths.build_command = 'cmake -D CMAKE_BUILD_TYPE=RelWithDebInfo -D CMAKE_CXX_COMPILER=hcc -DTensile_ROOT=$(pwd)/../Tensile ../HostLibraryTests'
+
     // Define test architectures, optional rocm version argument is available
-    def nodes = new dockerNodes(['gfx900 && ubuntu', 'gfx906 && ubuntu', 'gfx908 && ubuntu'], tensile)
+    def nodes = new dockerNodes(nodeDetails, jobName, prj)
 
     boolean formatCheck = false
 
@@ -43,4 +46,43 @@ tensileCI:
 
     buildProject(tensile, formatCheck, nodes.dockerArray, compileCommand, testCommand, null)
 
+}
+
+ci: {
+    String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
+
+    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 1 * * 0')])],
+                        "compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 1 * * 0')])],
+                        "rocm-docker":[]]
+    propertyList = auxiliary.appendPropertyList(propertyList)
+
+    Set standardJobNameSet = ["compute-rocm-dkms-no-npi", "compute-rocm-dkms-no-npi-hipclang", "rocm-docker"]
+
+    def jobNameList = ["compute-rocm-dkms-no-npi":([ubuntu16:['gfx900'],ubuntu16:['gfx906'],ubuntu16:['gfx908']]),
+                       "compute-rocm-dkms-no-npi-hipclang":([ubuntu16:['gfx900'],ubuntu16:['gfx906'],ubuntu16:['gfx908']]),
+                       "rocm-docker":([ubuntu16:['gfx900'],ubuntu16:['gfx906'],ubuntu16:['gfx908']])]
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
+
+    propertyList.each
+    {
+        jobName, property->
+        if (urlJobName == jobName)
+            properties(auxiliary.addCommonProperties(property))
+    }
+
+    Set seenJobNames = []
+    jobNameList.each
+    {
+        jobName, nodeDetails->
+        seenJobNames.add(jobName)
+        if (urlJobName == jobName)
+            runCI(nodeDetails, jobName)
+    }
+
+    // For url job names that are outside of the standardJobNameSet i.e. compute-rocm-dkms-no-npi-1901
+    if(!seenJobNames.contains(urlJobName))
+    {
+        properties(auxiliary.addCommonProperties([pipelineTriggers([cron('0 1 * * *')])]))
+        runCI([ubuntu16:['gfx906']], urlJobName)
+    }
 }
