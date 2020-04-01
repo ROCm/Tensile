64f23ceca8928314b7d6f52b13d6bdc3aa41288e
Ben Sander <ben.sander@amd.com>
2020-03-31 21:31:32 -0400

    refactor parse_results - add derived stats, perf model

--- bin/parse_results.py
+++ bin/parse_results.py
@@ -3,36 +3,62 @@
 import sys,fileinput,csv
 import argparse
 from collections import OrderedDict
+from functools import reduce
+import operator
+
+class DataRow:
+    """ Single result row """
+    def __init__(self, row):
+        self.dataRow = row
+        self.derivedFields = {}
+
+    def __getitem__(self, key):
+        return self.dataRow[key]
+
+    def finalizeDerivedFields(self, solution):
+        self.derivedFields['best-any'] = str(solution.maxPerfRow["gflops"])
+        if args.ops_per_cu_per_cycle:
+            peakMFlops = float(self["clock-sys"]) * float(self["num-cus"]) * args.ops_per_cu_per_cycle
+            sizes = [int(p) for p in self['problem-sizes'].strip("()").split(",")]
+            work = reduce(operator.mul, sizes, 2.0)
+            self.derivedFields['model-alu-us'] = '%6.0f' % (work / peakMFlops * float(self["total-granularity"]))
+        if args.mem_width:
+            peakMBps = float(self["clock-mem"]) * args.mem_width * args.mem_eff/100.0
+            self.derivedFields['model-mem-read-us']  = "%6.0f" % (float(self["mem-read-bytes"]) / peakMBps)
+            self.derivedFields['model-mem-write-us'] = "%6.0f" % (float(self["mem-write-bytes"]) / peakMBps)
+
 
 class Solution:
     """ Rows associated with this solution """
     def __init__(self):
         self.problemResults = []
-        self.maxPerf = None
+        self.maxPerfRow = None
 
     def addRow(self,row):
         self.problemResults.append(row)
-        if self.maxPerf==None or float(row["gflops"]) > float(self.maxPerf["gflops"]):
-            self.maxPerf = row
+        if self.maxPerfRow==None or float(row["gflops"]) > float(self.maxPerfRow["gflops"]):
+            self.maxPerfRow = row
 
 class ProblemSize:
     """ Rows associated with this problem """
     def __init__(self):
-        self.rows = []
+        self.dataRows = []
 
     def addRow(self,row):
-        self.rows.append(row)
+        self.dataRows.append(row)
 
 
 class Reader:
 
-    def formatCol(self, row, colName, val=None):
-        return ('{0: <%d}'%self.maxLen[colName]).format(val if val else row[colName])
+    def formatCol(self, fieldName, val):
+        return ('{0: <%d}'%self.fieldLength[fieldName]).format(val)
 
-    def printRow(self, row, fieldsToPrint, end='\n'):
+    def printRow(self, row, fieldsToPrint, derivedFieldsToPrint, end='\n'):
         for field in fieldsToPrint:
-            print (self.formatCol(row,field), end=" ")
-        print (self.solutions[row["solution"]].maxPerf["gflops"], end="") # max perf from any solution
+            print (self.formatCol(field, row[field]), end=" ")
+
+        for field in derivedFieldsToPrint:
+            print (self.formatCol(field, row.derivedFields[field]), end=" ")
         print (end=end)
 
 
@@ -51,50 +77,66 @@ class Reader:
 
         dictReader = csv.DictReader(csv_table, csv_field_names, skipinitialspace=True)
 
-        fieldsToPrint= ('problem-progress', 'problem-sizes', 'solution', 'time-us', 'gflops', 'alu-us', 'mem-read-us', 'mem-write-us', 'tiles-per-cu', 'total-granularity', 'cu-granularity', 'tile0-granularity', 'tile1-granularity')
-        derivedFields = ['best-any']
+        fieldsToPrint= ('problem-progress', 'problem-sizes', 'solution', 'time-us', 'gflops', \
+                        'tiles-per-cu', 'total-granularity', 'cu-granularity', 'tile0-granularity', 'tile1-granularity')
+        derivedFieldsToPrint = []
+        if args.ops_per_cu_per_cycle:
+            derivedFieldsToPrint += ["model-alu-us"]
+        if args.mem_width:
+            derivedFieldsToPrint += ["model-mem-read-us", "model-mem-write-us"]
+        derivedFieldsToPrint += ['best-any']
 
-        self.maxLen={}
+        for f in fieldsToPrint:
+            assert f not in derivedFieldsToPrint
+
+        self.fieldLength={}
         for k in fieldsToPrint:
-            self.maxLen[k] = len(k) # length of name
+            self.fieldLength[k] = len(k) # length of name
+        for k in derivedFieldsToPrint:
+            self.fieldLength[k] = len(k)
 
         self.dataOut=[]
         self.solutions = {} # map from solution to Solution class
         problemSizes = OrderedDict()
         for row in dictReader:
-            self.solutions.setdefault(row["solution"],Solution()).addRow(row)
-            problemSizes.setdefault(row["problem-sizes"],ProblemSize()).addRow(row)
+            self.solutions.setdefault(row["solution"],Solution()).addRow(DataRow(row))
+            problemSizes.setdefault(row["problem-sizes"],ProblemSize()).addRow(DataRow(row))
 
             keep = {}
             for k in fieldsToPrint:
                 keep[k] = row[k]
-                self.maxLen[k] = max(len(row[k]), self.maxLen[k])
+                self.fieldLength[k] = max(len(row[k]), self.fieldLength[k])
 
             self.dataOut.append((keep))
+
+        # after everything added, compute derived stats:
+        for key,ps in problemSizes.items():
+            for row in ps.dataRows:
+                row.finalizeDerivedFields(self.solutions[row["solution"]])
+                for k in derivedFieldsToPrint:
+                    self.fieldLength[k] = max(len(row.derivedFields[k]), self.fieldLength[k])
 
         # sort by gflops
         self.dataOut.sort(key = lambda row: int(row["gflops"]), reverse=True)
 
         # print header:
         for field in fieldsToPrint:
-            print (self.formatCol(row,field,val=field), end=" ")
-        for field in derivedFields:
+            print (self.formatCol(field,val=field), end=" ")
+        for field in derivedFieldsToPrint:
             print (field,end=" ")
         print()
 
         if args.print_winners:
             for key,ps in problemSizes.items():
-                if args.problem_progress==None or int(ps.rows[0]["problem-progress"].split('/')[0]) in args.problem_progress:
-                    rows = [row for row in ps.rows if args.filter in row["solution"]]
-                    sortedRows = sorted(rows, key = lambda row: int(row["gflops"]), reverse=True)
+                if args.problem_progress==None or int(ps.dataRows[0]["problem-progress"].split('/')[0]) in args.problem_progress:
+                    dataRows = [row for row in ps.dataRows if args.filter in row["solution"]]
+                    sortedRows = sorted(dataRows, key = lambda row: int(row["gflops"]), reverse=True)
                     for i in range(min(len(sortedRows),args.print_winners) if args.print_winners>=0 else len(sortedRows)):
-                        self.printRow(sortedRows[i], fieldsToPrint)
+                        self.printRow(sortedRows[i], fieldsToPrint, derivedFieldsToPrint)
 
         if args.print_summary:
             for row in self.dataOut:
-                self.printRow(row, fieldsToPrint)
-
-            #print (self.formatCol(row,'problem-sizes'), self.formatCol(row, 'solution'), " %6.1f %6d" %(float(row['time-us']), int(row['gflops'])))
+                self.printRow(row, fieldsToPrint, derivedFieldsToPrint)
 
 
 
@@ -108,6 +150,12 @@ my_parser.add_argument('--print_summary', '-s', action='store_true',
                        help='print all results')
 my_parser.add_argument('--filter', '-k', type=str, default="",
                        help='filter solutions, ie "GSU1".')
+my_parser.add_argument('--ops-per-cu-per-cycle', '-o', type=int, default=0,
+                       help='ops per cu per cycle. Typical values 64(vega20 f64), 128(vega20 f32), etc. used to compute AluUs')
+my_parser.add_argument('--mem-width', '-m', type=int, default=1024,
+                       help='mem bus width in bits. Typical values 1024 (vega20).  Used to compute Mem*Us')
+my_parser.add_argument('--mem-eff', '-e', type=int, default=85, choices=range(1,100),
+                       help='efficiency of memory bus.  Must be integer 1.100.')
 
 my_parser.add_argument('input_file', help='file with tensile output to parse')
 
