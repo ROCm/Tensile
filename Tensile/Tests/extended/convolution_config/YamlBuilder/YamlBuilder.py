23f0f6b02d41f2fcb0f79ba9ab5b7b15a9819a85
zaliu <35415350+zaliu@users.noreply.github.com>
2020-04-01 14:23:51 -0700

    Revert "Workaround vector store"

--- Tensile/Tests/extended/convolution_config/YamlBuilder/YamlBuilder.py
+++ Tensile/Tests/extended/convolution_config/YamlBuilder/YamlBuilder.py
@@ -115,7 +115,6 @@ class Solutions:
                     {"VectorWidth": [1,4]},
                     {"FractionalLoad": [0,1]},
                     {"PackBatchDims": [0,1]},
-                    {"VectorStore": [1]}, # TODO - remove when VectorStore=0 + PBD bug fixed
                 ]
 
         return s
