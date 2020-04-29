7ebf34be5aad7999d98a1ac4b5224c1f734fc10e
Eiden Yoshida <eiden.yoshida@amd.com>
2020-04-28 22:41:13 -0600

    Remove other OSes from CI

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -48,8 +48,8 @@ def runCI =
 ci: { 
     String urlJobName = auxiliary.getTopJobName(env.BUILD_URL)
 
-    def propertyList = ["compute-rocm-dkms-no-npi":[pipelineTriggers([cron('0 6 * * 6')])], 
-                        "compute-rocm-dkms-no-npi-hipclang":[pipelineTriggers([cron('0 6 * * 6')])],
+    def propertyList = ["compute-rocm-dkms-no-npi":[]],
+                        "compute-rocm-dkms-no-npi-hipclang":[]],
                         "rocm-docker":[]]
     propertyList = auxiliary.appendPropertyList(propertyList)
 
@@ -57,7 +57,7 @@ ci: {
                        "compute-rocm-dkms-no-npi-hipclang":([ubuntu16:['gfx900','gfx906','gfx908']]), 
                        "rocm-docker":([ubuntu16:['gfx900','gfx906','gfx908']])]
 
-    jobNameList = auxiliary.appendJobNameList(jobNameList)
+    // jobNameList = auxiliary.appendJobNameList(jobNameList)
 
     propertyList.each 
     {
