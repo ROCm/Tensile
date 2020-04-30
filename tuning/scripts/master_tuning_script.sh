4fd0c1fb8f8ccce7dabbc3e1d31ca666363c2184
Akila Premachandra <akila.premachandra@amd.com>
2020-04-30 11:28:42 -0600

    Fixes to scrip

--- tuning/scripts/master_tuning_script.sh
+++ tuning/scripts/master_tuning_script.sh
@@ -1,7 +1,36 @@
 #!/usr/bin/env bash
 
-HELP_STR="usage: $0 [-o|--output-dir] [-y|--data-type (sgemm,dgemm,hgemm only)] [-z|--log] [-g|--gpu (arcturus,mi25,mi50,mi60,v340)] [-m|--mfma] 
-                    [-r|--rk] [-f|--sclk] [-c|--count] [-t|--tile-aware] [-h|--help]"
+HELP_STR="
+    Pre-Requisites: >=Anaconda 3.6 (or install python3.6 or higher, python3-pip/pip3, python3-yaml, python3-setuptools, python3-distutils python3-venv, wheel, setuptools, pyyaml, matplotlib, pandas, and numpy)
+                    >=llvm-6.0-dev, >=cmake3.5, zlib1g-dev
+                    <=rocm3.0 stack for hcc, >=rocm3.3 stack for hip-clang
+    About
+    This is the master tuning script. Here's what it does: 
+    1) Generates an input yaml file (ROCBLAS_LAYER=2 and ROCBLAS_LAYER=4 are supported) for each of the NN/NT/TN configurations.
+    2) Runs Tensile for all 3 input yaml files
+    3) Merges the tuning results (yaml file in 3_LibraryLogic directory) into the existing rocBLAS
+    4) Runs massage script (adds ldc=ldd sizes) for vega10 and vega20 only
+    5) Builds rocBLAS without tuned sizes (build_dir=reference_build)
+    6) Builds rocBLAS with tuned sizes (build_dir=tuned_build)
+    7) Runs rocblas-bench with untuned sizes and tuned sizes
+    8) Runs analysis script, providing spreadsheets with performance before and after tuning
+
+    usage: $0 
+    [-h|--help]         Display this help message
+    [-o|--output-dir]   Output directory for all tuning-related files
+    [-y|--data-type]    Data type of sizes that you want to tune (sgemm, dgemm, hgemm only)
+    [-g|--gpu]          GPU used for tuning (arcturus, mi25, mi50, mi60, r7, v340 only)
+    [-f|--sclk]         Frequency of sclk in MHz 
+    [-z|--log]          Pass in log file with rocblas-bench calls, or directory of log files if using network tuning
+    [-n|--network]      Optional. String to search for in filenames in log directory
+    [-m|--mfma]         Optional. Use MFMA kernels (default=false)
+    [-r|--rk]           Optional. Use replacement kernels (sgemm only, default=false)
+    [-c|--count]        Optional. Sets all cases where count=1 to count=10 (default=false)
+    [-t|--tile-aware]   Optional. Use tile-aware method. (limited support, default=false) 
+    [--number]          Optional. Set script number (view scripts/performance in rocBLAS directory, default=1)
+    [-u|--username]     Optional. Specify which rocBLAS/Tensile fork to use (default=ROCmSoftwarePlatform)
+    [-b|--branch]       Optional. Specify which rocBLAS/Tensile branch to use (default=develop)
+"
 HELP=false
 COUNT=false
 TILE_AWARE=false
@@ -64,7 +93,7 @@ elif [[ "${DATA_TYPE}" == dgemm || "${DATA_TYPE}" == d ]]; then
     DATA_TYPE=dgemm
     DVAL=1
 else
-    printf "Data Type not specified, assuming sgemm\n"
+    printf "Assuming sgemm\n"
     DATA_TYPE=sgemm
     DVAL=2
 fi
@@ -73,12 +102,14 @@ if [[ "${GPU}" == mi25 || "${GPU}" == v340 ]]; then
     LIBRARY=vega10
 elif [[ "${GPU}" == arcturus ]]; then
     LIBRARY=arcturus
+elif [[ "${GPU}" == mi50 || "${GPU}" == r7 ]]; then
+    GPU=mi50
 else
-    printf "Assuming vega20 gpu library, refer to help string if this is inaccurate\n"
+    printf "Assuming vega20 gpu library\n"
     GPU=mi60
 fi
 
-make_tune_all_scripts () {
+run_tune_all_scripts () {
     NN=build-${LIBRARY}-${DATA_TYPE}-nn-${OUTPUT_DIR}
     NT=build-${LIBRARY}-${DATA_TYPE}-nt-${OUTPUT_DIR}
     TN=build-${LIBRARY}-${DATA_TYPE}-tn-${OUTPUT_DIR}
@@ -102,7 +133,7 @@ make_tune_all_scripts () {
     pushd ${NN}
     echo "#!/bin/sh" > tune.sh
     echo "touch time.begin" >> tune.sh
-    echo "../Tensile/bin/Tensile $NN ./ > make.out 2>&1" >> tune.sh
+    echo "../Tensile/bin/Tensile ${LIBRARY}_${DATA_TYPE}_nn_${OUTPUT_DIR}.yaml ./ 2>&1 | tee make.out" >> tune.sh
     echo "touch time.end" >> tune.sh
     chmod 755 tune.sh
     popd
@@ -110,7 +141,7 @@ make_tune_all_scripts () {
     pushd ${NT}
     echo "#!/bin/sh" > tune.sh
     echo "touch time.begin" >> tune.sh
-    echo "../Tensile/bin/Tensile $NT ./ > make.out 2>&1" >> tune.sh
+    echo "../Tensile/bin/Tensile ${LIBRARY}_${DATA_TYPE}_nt_${OUTPUT_DIR}.yaml ./ 2>&1 | tee make.out" >> tune.sh
     echo "touch time.end" >> tune.sh
     chmod 755 tune.sh
     popd
@@ -118,11 +149,13 @@ make_tune_all_scripts () {
     pushd ${TN}
     echo "#!/bin/sh" > tune.sh
     echo "touch time.begin" >> tune.sh
-    echo "../Tensile/bin/Tensile $TN ./ > make.out 2>&1" >> tune.sh
+    echo "../Tensile/bin/Tensile ${LIBRARY}_${DATA_TYPE}_tn_${OUTPUT_DIR}.yaml ./ 2>&1 | tee make.out" >> tune.sh
     echo "touch time.end" >> tune.sh
     chmod 755 tune.sh
     popd
 
+    ./tune-all.sh
+    
     mkdir exact
     cp ${NN}/3_LibraryLogic/* exact/
     cp ${NT}/3_LibraryLogic/* exact/
@@ -141,23 +174,23 @@ pushd ${OUTPUT_DIR}
 chmod 755 scripts/*
 chmod 755 scripts2/*
 git clone https://github.com/${ORGANIZATION}/Tensile.git -b ${BRANCH}
+
 pushd Tensile
-make_tune_all_scripts
-./tune-all.sh
+run_tune_all_scripts
 popd
 
 git clone https://github.com/${ORGANIZATION}/rocBLAS-internal.git -b ${BRANCH} rocBLAS
 mkdir library
 mv Tensile/exact library/
 mkdir library/merge
-python Tensile/Utilities/merge_rocblas_yaml_files.py rocBLAS/library/src/blas3/Tensile/Logic/archive library/exact library/merge
+python Tensile/Tensile/Utilities/merge_rocblas_yaml_files.py rocBLAS/library/src/blas3/Tensile/Logic/archive library/exact library/merge
 
 if [[ "${LIBRARY}" != arcturus ]]; then
     python rocBLAS/library/src/blas3/Tensile/Logic/archive/massage.py library/merge library/massage
 fi
 
 pushd rocBLAS
-./install -c --build_dir benchmark-build 2>&1 | tee log-benchmark-build
+./install.sh -c --build_dir reference-build 2>&1 | tee log-reference-build
 
 if [[ "${LIBRARY}" != arcturus ]]; then
     cp ../library/massage/* library/src/blas3/Tensile/Logic/asm_full
@@ -166,11 +199,11 @@ if [[ "${LIBRARY}" != arcturus ]]; then
 else
     cp ../library/merge/* library/src/blas3/Tensile/Logic/asm_full
 fi
-./install -c --build_dir tuned-build 2>&1 | tee log-tuned-build
+./install.sh -c --build_dir tuned-build 2>&1 | tee log-tuned-build
 
-cp ../scripts/*.sh benchmark-build/release/clients/staging
+cp ../scripts/*.sh reference-build/release/clients/staging
 cp ../scripts/*.sh tuned-build/release/clients/staging
-pushd benchmark-build/release/clients/staging
+pushd reference-build/release/clients/staging
 ./doit_all1.sh
 find results1 -name \*.1 -exec sed -i "s/4t/t/g" {} \;
 popd
@@ -185,4 +218,4 @@ popd
 popd
 
 source ~/.bashrc
-./tuning/scripts/analyze-results.sh -o ${OUTPUT_DIR}/analysis -r ${OUTPUT_DIR}/rocBLAS/benchmark-build/release/clients/staging/results1 -b ${OUTPUT_DIR}/rocBLAS/tuned-build/release/clients/staging/results1 -z ${LOG} -f ${SCLK} -s ${DVAL} -g ${GPU} -c ${COUNT} -m ${MFMA}
+./tuning/scripts/analyze-results.sh -o ${OUTPUT_DIR}/analysis -r ${OUTPUT_DIR}/rocBLAS/reference-build/release/clients/staging/results1 -b ${OUTPUT_DIR}/rocBLAS/tuned-build/release/clients/staging/results1 -z ${LOG} -f ${SCLK} -s ${DVAL} -g ${GPU} -c ${COUNT} -m ${MFMA}
