1e61ee76e89d6f5e622f1698c30af529e194efd8
billg <William.Gilmartin@amd.com>
2020-09-02 11:34:11 -0700

    Revert "fix clang_formatting"
    This reverts commit e14bf56b7b80a7c0cf1c66cb11f1df1936b08a44.

---
+++ HostLibraryTests/hip/RunGEMMKernelTileSelection_test.cpp
@@ -0,0 +1,245 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2019-2020 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#include <gtest/gtest.h>
+
+#include <Tensile/SolutionLibrary.hpp>
+
+#include <Tensile/hip/HipHardware.hpp>
+#include <Tensile/hip/HipSolutionAdapter.hpp>
+#include <Tensile/hip/HipUtils.hpp>
+
+#include <Tensile/AMDGPU.hpp>
+#include <Tensile/ContractionProblem.hpp>
+#include <Tensile/ContractionSolution.hpp>
+#include <Tensile/EmbeddedLibrary.hpp>
+#include <Tensile/Utils.hpp>
+
+#include "TestData.hpp"
+#include "TestUtils.hpp"
+
+#include <Reference.hpp>
+
+#include <cstddef>
+#include <random>
+
+using namespace Tensile;
+
+#define ASSERT_RB(exp) ASSERT_EQ((exp), rocblas_status_success)
+
+struct RunGEMMKernelSolutionSelectionTest : public ::testing::TestWithParam<ContractionProblem>
+{
+    
+    std::vector<float> a_h;
+    std::vector<float> b_h;
+    std::vector<float> c_h;
+    std::vector<float> d_h;
+    std::vector<float> d_in_h;
+    std::vector<float> d_ref_h;
+
+    float* a_d     = nullptr;
+    float* b_d     = nullptr;
+    float* c_d     = nullptr;
+    float* d_d     = nullptr;
+    float* d_ref_d = nullptr;
+
+    TypedContractionInputs<float> inputs;
+
+    std::shared_ptr<Hardware> hardware;
+
+    void SetUp() override
+    {
+        HIP_CHECK_EXC(hipSetDevice(0));
+        ContractionProblem problem = GetParam();
+        std::cout << problem << std::endl;
+
+        a_h.resize(problem.a().totalAllocatedElements());
+        b_h.resize(problem.b().totalAllocatedElements());
+        c_h.resize(problem.c().totalAllocatedElements());
+        d_h.resize(problem.d().totalAllocatedElements());
+        d_in_h.resize(problem.d().totalAllocatedElements());
+
+        std::mt19937 rng;
+
+
+        InitTensor(a_h.data(), problem.a(), RandomInt<float>(), rng);
+        InitTensor(b_h.data(), problem.b(), RandomAlternatingInt<float>(), rng);
+        InitTensor(c_h.data(), problem.c(), RandomInt<float>(), rng);
+        InitTensor(d_in_h.data(), problem.d(), RandomInt<float>(), rng);
+
+        d_ref_h = d_h;
+
+        CopyTensor(d_ref_h.data(), c_h.data(), problem.d(), problem.c());
+
+        HIP_CHECK_EXC(hipMalloc(&a_d, problem.a().totalAllocatedBytes()));
+        HIP_CHECK_EXC(hipMalloc(&b_d, problem.b().totalAllocatedBytes()));
+        HIP_CHECK_EXC(hipMalloc(&c_d, problem.c().totalAllocatedBytes()));
+        HIP_CHECK_EXC(hipMalloc(&d_d, problem.d().totalAllocatedBytes()));
+        HIP_CHECK_EXC(hipMalloc(&d_ref_d, problem.d().totalAllocatedBytes()));
+
+        HIP_CHECK_EXC(
+            hipMemcpy(a_d, a_h.data(), problem.a().totalAllocatedBytes(), hipMemcpyHostToDevice));
+        HIP_CHECK_EXC(
+            hipMemcpy(b_d, b_h.data(), problem.b().totalAllocatedBytes(), hipMemcpyHostToDevice));
+        HIP_CHECK_EXC(
+            hipMemcpy(c_d, c_h.data(), problem.c().totalAllocatedBytes(), hipMemcpyHostToDevice));
+        HIP_CHECK_EXC(hipMemcpy(
+            d_d, d_in_h.data(), problem.d().totalAllocatedBytes(), hipMemcpyHostToDevice));
+        HIP_CHECK_EXC(hipMemcpy(
+            d_ref_d, d_ref_h.data(), problem.d().totalAllocatedBytes(), hipMemcpyHostToDevice));
+
+        inputs.a = a_d;
+        inputs.b = b_d;
+        inputs.c = c_d;
+        inputs.d = d_d;
+
+        inputs.alpha = RandomInt<float>()(rng);
+        if(problem.beta() == 1.0)
+            inputs.beta = 1.0;
+        else if(problem.beta() == 0.0)
+            inputs.beta = 0.0;
+        else
+            inputs.beta = RandomInt<float>()(rng);
+
+        hardware = hip::GetCurrentDevice();
+        ASSERT_NE(hardware, nullptr);
+
+        TypedContractionInputs<float> inputsRefHost;
+        inputsRefHost.a     = a_h.data();
+        inputsRefHost.b     = b_h.data();
+        inputsRefHost.c     = c_h.data();
+        inputsRefHost.d     = d_ref_h.data();
+        inputsRefHost.alpha = inputs.alpha;
+        inputsRefHost.beta  = inputs.beta;
+
+        Client::SolveCPU(problem, inputsRefHost);
+    }
+
+    void TearDown() override
+    {
+        hipFree(a_d);
+        hipFree(b_d);
+        hipFree(c_d);
+        hipFree(d_d);
+        hipFree(d_ref_d);
+
+        hipDeviceReset();
+    }
+};
+
+TEST_P(RunGEMMKernelSolutionSelectionTest, KernelsTileSelection)
+{
+    ContractionProblem problem = GetParam();
+
+    bool debug = false;
+    auto library = LoadLibraryFile<ContractionProblem>(
+            TestData::Instance().file("tile_aware_selection/library/TensileLibrary").native());
+
+    auto adapter = std::make_shared<hip::SolutionAdapter>(debug, "tile_aware_selection");
+    for(auto file : TestData::Instance().glob("tile_aware_selection/library/*.*co"))
+        adapter->loadCodeObjectFile(file.native());
+
+    for(auto file : TestData::Instance().glob("tile_aware_selection/library/*.*hsaco"))
+        adapter->loadCodeObjectFile(file.native());
+
+    ASSERT_NE(library, nullptr);
+
+    auto solution = library->findBestSolution(problem, *hardware);
+
+    ASSERT_NE(solution, nullptr);
+
+    std::vector<KernelInvocation> result = solution->solve(problem, inputs, *hardware);
+
+    adapter->launchKernels(result);
+
+    HIP_CHECK_EXC(
+        hipMemcpy(d_h.data(), d_d, problem.d().totalAllocatedBytes(), hipMemcpyDeviceToHost));
+
+    for(int i = 0; i < d_ref_h.size(); i++)
+    {
+        ASSERT_FLOAT_EQ(d_h[i], d_ref_h[i]) << i;
+    }
+
+}
+
+TEST_P(RunGEMMKernelSolutionSelectionTest, TileAwareMetricSelection)
+{
+
+    ContractionProblem problem = GetParam();
+
+    bool debug = false;
+    auto library = LoadLibraryFile<ContractionProblem>(
+            TestData::Instance().file("tile_aware_metric_selection/library/TensileLibrary").native());
+
+    auto adapter = std::make_shared<hip::SolutionAdapter>(debug, "fitness_selection");
+    for(auto file : TestData::Instance().glob("tile_aware_metric_selection/library/*.*co"))
+        adapter->loadCodeObjectFile(file.native());
+
+    ASSERT_NE(library, nullptr);
+
+
+    auto solution = library->findBestSolution(problem, *hardware);
+
+    ASSERT_NE(solution, nullptr);
+
+    std::vector<KernelInvocation> result = solution->solve(problem, inputs, *hardware);
+
+    adapter->launchKernels(result);
+
+    HIP_CHECK_EXC(
+        hipMemcpy(d_h.data(), d_d, problem.d().totalAllocatedBytes(), hipMemcpyDeviceToHost));
+
+    for(int i = 0; i < d_ref_h.size(); i++)
+    {
+        ASSERT_FLOAT_EQ(d_h[i], d_ref_h[i]) << i;
+    }
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    HipSolutionAdapter,
+    RunGEMMKernelSolutionSelectionTest,
+    ::testing::Values(
+        ContractionProblem::GEMM(false, false, 4, 4, 6, 4, 6, 4, 1.5, false, 2),
+        ContractionProblem::GEMM(false, true, 4, 4, 6, 4, 4, 4, 1.5, false, 2),
+        ContractionProblem::GEMM(true, false, 4, 4, 6, 6, 6, 4, 1.5, false, 2),
+        ContractionProblem::GEMM(true, true, 4, 4, 6, 6, 4, 4, 1.5, false, 2),
+        ContractionProblem::GEMM(false, false, 234, 123, 634, 234, 634, 234, 1.5, false, 1),
+        ContractionProblem::GEMM(false, false, 234, 123, 634, 245, 768, 249, 1.5, false, 12),
+        ContractionProblem::GEMM(false, true, 234, 123, 634, 245, 768, 249, 1.5, false, 12),
+        ContractionProblem::GEMM(true, false, 234, 123, 634, 768, 768, 249, 1.5, false, 12),
+        ContractionProblem::GEMM(true, true, 234, 123, 634, 768, 768, 249, 1.5, false, 12)//,
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM(),
+        //RandomGEMM()
+        ));
+
+       
---
+++ Tensile/Source/lib/include/Tensile/ContractionSolution.hpp
@@ -0,0 +1,317 @@
+/**
+ * Copyright 2019-2020 Advanced Micro Devices, Inc. All rights reserved.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+#pragma once
+
+#include <cstddef>
+#include <limits>
+#include <memory>
+#include <string>
+#include <vector>
+
+#include <Tensile/Tensile.hpp>
+
+#include <Tensile/ContractionProblem_fwd.hpp>
+#include <Tensile/DataTypes.hpp>
+#include <Tensile/Predicates.hpp>
+
+namespace Tensile
+{
+    struct PerfModel
+    {
+        double clock            = std::numeric_limits<double>::quiet_NaN();
+        double memClock         = std::numeric_limits<double>::quiet_NaN();
+        double peakGFlops       = std::numeric_limits<double>::quiet_NaN();
+        double memBandwidthMBps = std::numeric_limits<double>::quiet_NaN();
+        double l2ReadBwMul      = std::numeric_limits<double>::quiet_NaN();
+        double gFlops           = std::numeric_limits<double>::quiet_NaN();
+        double readEff          = 0.0;
+        double l2ReadHitRate    = 0.0;
+        double l2WriteHitRate   = 0.0;
+        int    CUs              = 0;
+    };
+
+    extern PerfModel perf;
+
+    struct BufferLoadCheckPacket
+    {
+        size_t shiftPtrElemA;
+        size_t shiftPtrElemB;
+        size_t depthUorMT0;
+        size_t depthUorMT1;
+    };
+
+    /**
+ * Represents a single kernel or set of kernels that can perform a single
+ * tensor contraction.
+ *
+ * Can generate `KernelInvocation` objects to solve a particular problem
+ * given a set of `ContractionInputs`.
+ */
+    class ContractionSolution : public Solution
+    {
+    public:
+        using Problem = ContractionProblem;
+        using Inputs  = ContractionInputs;
+
+        static std::string Type()
+        {
+            return "Contraction";
+        }
+        virtual std::string type() const
+        {
+            return Type();
+        }
+
+        virtual std::string KernelName() const
+        {
+            return kernelName;
+        }
+        virtual std::string name() const
+        {
+            return kernelName;
+        }
+        virtual std::string description() const
+        {
+            return kernelName;
+        }
+
+        bool isSourceKernel() const;
+
+
+        struct StaticTAMetricPerformanceModel
+        {
+            size_t memReadBytesA   = 0.0; //! Estimated memory reads A
+            size_t memReadBytesB   = 0.0; //! Estimated memory reads B
+            size_t memReadBytesC   = 0.0; //! Estimated memory reads C
+            size_t memWriteBytesD  = 0.0; //! Estimated memory writes D
+            size_t memReadBytes    = 0.0;
+            size_t memGlobalReads  = 0;
+            size_t memGlobalWrites = 0;
+        };
+
+        struct TAMetricProblemScore
+        {
+            double numTiles0  = 0.0; //! number of tiles in 0 dimension
+            double numTiles1  = 0.0; //! number of tiles in 1 dimension
+            double totalTiles = 0.0;
+            double tilesPerCu = 0.0;
+
+            //! Granularity is measured 0..1 with 1.0 meaning no granularity loss
+            double tile0Granularity = 0.0; // loss due to tile0
+            double tile1Granularity = 0.0;
+            double cuGranularity    = 0.0;
+            double waveGranularity  = 0.0;
+            double totalGranularity = 0.0;
+            double suTilesPerCu = 0.0;
+            double suCuGranularity = 0.0;
+            double waves = 0.0;
+            double suWavesPerSimdx2 = 0.0;
+            double suWaveGranularity = 0.0;
+
+            double speedGFlops = 0.0; //! final gflops projection
+            int    CUs         = 0;
+
+            //StaticTAMetricPerformanceModel staticModel;
+        };
+
+        StaticTAMetricPerformanceModel staticTAMetricPerformanceModel(double M,
+                                                      double N,
+                                                      double K,
+                                                      double NumBatches,
+                                                      double MT0,
+                                                      double MT1,
+                                                      double NumCUs,
+                                                      double totalGranularity,
+                                                      int    globalSplitU) const;
+
+
+        TAMetricProblemScore computeProblemScore(
+            Hardware const& hardware, 
+            double M, double N, double K, double NumBatches,
+            double LDA, double LDB, double LDC, double LDD) const;
+
+        /**
+        * Calculate the projected performance based on granularity loss.
+        */
+        TAMetricProblemScore projectedTAMetricPerformance(Problem const&  problem,
+                                                  Hardware const& hardware) const;
+
+        //! Estimates based on problem size, solution tile, and  machine hardware
+        //! charz:
+        struct StaticPerformanceModel
+        {
+            size_t memReadBytesA   = 0.0; //! Estimated memory reads A
+            size_t memReadBytesB   = 0.0; //! Estimated memory reads B
+            size_t memReadBytesC   = 0.0; //! Estimated memory reads C
+            size_t memWriteBytesD  = 0.0; //! Estimated memory writes D
+            size_t memReadBytes    = 0.0;
+            size_t memGlobalReads  = 0;
+            size_t memGlobalWrites = 0;
+        };
+
+        struct ProjectedPerformance
+        {
+            double numTiles0  = 0.0; //! number of tiles in 0 dimension
+            double numTiles1  = 0.0; //! number of tiles in 1 dimension
+            double tilesPerCu = 0.0;
+
+            //! Granularity is measured 0..1 with 1.0 meaning no granularity loss
+            double tile0Granularity = 0.0; // loss due to tile0
+            double tile1Granularity = 0.0;
+            double cuGranularity    = 0.0;
+            double waveGranularity  = 0.0;
+            double totalGranularity = 0.0;
+
+            double speedGFlops = 0.0; //! final gflops projection
+            int    CUs         = 0;
+
+            StaticPerformanceModel staticModel;
+        };
+
+        /**
+   * Calculate required workspace size.
+   */
+        size_t requiredWorkspaceSize(Problem const& problem) const;
+
+        StaticPerformanceModel staticPerformanceModel(double M,
+                                                      double N,
+                                                      double K,
+                                                      double NumBatches,
+                                                      double MT0,
+                                                      double MT1,
+                                                      double NumCUs,
+                                                      double totalGranularity,
+                                                      int    globalSplitU) const;
+
+        /**
+   * Calculate the projected performance based on granularity loss.
+   */
+        ProjectedPerformance projectedPerformance(Problem const&  problem,
+                                                  Hardware const& hardware) const;
+
+        /**
+   * Generate a set of kernel calls to solve a particular problem.
+   */
+        virtual std::vector<KernelInvocation>
+            solve(Problem const& problem, Inputs const& inputs, Hardware const& hardware) const;
+
+        template <typename TypedInputs>
+        std::vector<KernelInvocation> solveTyped(Problem const&     problem,
+                                                 TypedInputs const& inputs,
+                                                 Hardware const&    hardware) const;
+
+        template <typename TypedInputs, bool T_Debug>
+        KernelInvocation generateSingleCall(Problem const&     problem,
+                                            TypedInputs const& inputs,
+                                            Hardware const&    hardware) const;
+
+        template <typename TypedInputs, bool T_Debug>
+        KernelInvocation generateBetaOnlyCall(Problem const&     problem,
+                                              TypedInputs const& inputs,
+                                              Hardware const&    hardware) const;
+
+        template <typename TypedInputs>
+        std::string betaOnlyKernelName(Problem const&     problem,
+                                       TypedInputs const& inputs,
+                                       Hardware const&    hardware) const;
+
+        template <typename TypedInputs>
+        KernelInvocation generateOutputConversionCall(Problem const&     problem,
+                                                      TypedInputs const& inputs,
+                                                      Hardware const&    hardware) const;
+
+        template <typename TypedInputs>
+        std::string outputConversionKernelName(Problem const&     problem,
+                                               TypedInputs const& inputs,
+                                               Hardware const&    hardware) const;
+
+        struct SizeMapping
+        {
+            dim3 workGroupSize;
+            dim3 threadTile;
+            dim3 macroTile;
+
+            size_t staggerU           = 0;
+            size_t depthU             = 0;
+            size_t globalSplitU       = 0;
+            size_t staggerStrideShift = 0;
+            int    workGroupMapping   = 0;
+
+            size_t packBatchDims     = 0;
+            int    packSummationDims = 0;
+            int    magicDivAlg       = 1;
+            size_t persistentKernel  = 0;
+
+            bool sourceKernel = false;
+
+            bool   globalAccumulation    = false;
+            size_t workspaceSizePerElemC = 0;
+        };
+
+        struct ProblemType
+        {
+            std::string operationIdentifier;
+            DataType    aType                   = DataType::Float;
+            DataType    bType                   = DataType::Float;
+            DataType    cType                   = DataType::Float;
+            DataType    dType                   = DataType::Float;
+            bool        highPrecisionAccumulate = false;
+            bool        useBeta                 = true;
+            bool        useInitialStridesAB     = false;
+            bool        useInitialStridesCD     = false;
+        };
+
+        int         index = 0;
+        std::string kernelName;
+        bool        debugKernel = false;
+
+        std::shared_ptr<Predicates::Predicate<Problem>> problemPredicate
+            = std::make_shared<Predicates::True<Problem>>();
+        std::shared_ptr<Predicates::Predicate<Hardware>> hardwarePredicate
+            = std::make_shared<Predicates::True<Hardware>>();
+
+        SizeMapping sizeMapping;
+
+        ProblemType problemType;
+
+        /// Debugging purposes.  Shouldn't contain any vital information that isn't
+        /// somewhere else.
+        std::map<std::string, std::string> info;
+        std::map<int, double>              ideals;
+
+        int32_t staggerUIter(Problem const&  problem,
+                             Inputs const&   inputs,
+                             Hardware const& hardware) const;
+
+        uint32_t magicNumberAlg1(uint32_t x, uint32_t* magicShift) const;
+        uint32_t magicNumberAlg2(uint32_t x, uint32_t* magicShift) const;
+        uint32_t magicNumber(int magicDivAlg, uint32_t x, uint32_t* magicShift) const;
+        uint32_t smallMagicNumber(uint32_t x) const;
+    };
+
+    std::ostream& operator<<(std::ostream&                                      stream,
+                             ContractionSolution::StaticPerformanceModel const& spm);
+    std::ostream& operator<<(std::ostream&                                    stream,
+                             ContractionSolution::ProjectedPerformance const& spm);
+    std::ostream& operator<<(std::ostream& stream, BufferLoadCheckPacket const& st);
+} // namespace Tensile
