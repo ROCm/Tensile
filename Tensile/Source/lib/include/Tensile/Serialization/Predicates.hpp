96b7861f71792d00c53ec8b0414046c767b8c796
Scott Quiring <scott.quiring@amd.com>
2020-09-08 15:52:27 -0600

    Include new enumeration values in serialization.

--- Tensile/Source/lib/include/Tensile/Serialization/Predicates.hpp
+++ Tensile/Source/lib/include/Tensile/Serialization/Predicates.hpp
@@ -211,6 +211,9 @@ namespace Tensile
                 iot::enumCase(io, value, "gfx906", AMDGPU::Processor::gfx906);
                 iot::enumCase(io, value, "gfx908", AMDGPU::Processor::gfx908);
                 iot::enumCase(io, value, "gfx1010", AMDGPU::Processor::gfx1010);
+                iot::enumCase(io, value, "gfx1011", AMDGPU::Processor::gfx1011);
+                iot::enumCase(io, value, "gfx1012", AMDGPU::Processor::gfx1012);
+                iot::enumCase(io, value, "gfx1030", AMDGPU::Processor::gfx1030);
             }
         };
     } // namespace Serialization
