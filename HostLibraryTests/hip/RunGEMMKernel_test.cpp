b28600e17a8161b8be82feaa06a12c029d040a81
Scott Quiring <scott.quiring@amd.com>
2020-10-06 23:17:32 -0600

    Only load relevant code objects.

--- HostLibraryTests/hip/RunGEMMKernel_test.cpp
+++ HostLibraryTests/hip/RunGEMMKernel_test.cpp
@@ -838,13 +838,15 @@ std::vector<std::tuple<std::shared_ptr<SolutionLibrary<ContractionProblem>>,
     {
         auto library = LoadLibraryFile<ContractionProblem>(envDir.file("TensileLibrary").native());
         auto adapter = std::make_shared<hip::SolutionAdapter>(debug, "TENSILE_TEST_LIBRARY");
+        auto device = std::dynamic_pointer_cast<AMDGPU>(Tensile::hip::GetCurrentDevice());
+        auto arch = device->processor;
 
-        for(auto file : envDir.glob("*.co"))
+        for(auto file : envDir.glob(concatenate("*-", arch, ".co")))
         {
             adapter->loadCodeObjectFile(file.native());
         }
 
-        for(auto file : envDir.glob("*.hsaco"))
+        for(auto file : envDir.glob(concatenate("*-", arch, ".hsaco")))
         {
             try
             {
