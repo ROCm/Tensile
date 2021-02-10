444a13dde92b0edded5a49d67aff9ba4d7c3ed22
Jeremy Hart <Jeremy.Hart@amd.com>
2021-02-10 21:36:00 +0000

    Added testing of MatrixInstruction parameter

--- Tensile/Utilities/merge.py
+++ Tensile/Utilities/merge.py
@@ -140,6 +140,9 @@ def findSolutionWithIndex(solutionData, solIndex):
         assert(len(solution) == 1)
         return solution[0]
 
+def usingMFMA(solution):
+    return solution["EnableMatrixInstruction"] or solution["MatrixInstruction"]
+
 class SolutionUpdate(Enum):
     Replace=0
     Append =1
@@ -154,7 +157,7 @@ def findOriginalSolutionToUpdate(incSolution, incSize, incEff, origSolutionData,
         if includeKernelVariants:
             for j, index, eff in origSolutions:
                 solution = findSolutionWithIndex(origSolutionData, index)
-                if solution["EnableMatrixInstruction"] == incSolution["EnableMatrixInstruction"]:
+                if usingMFMA(solution) == usingMFMA(incSolution):
                     origJ, origIndex, origEff = j, index, eff
                     originalSolution = solution
                     break
