a35a6e5ec8590abe671f0a0477178ef0e15836b7
Eiden Yoshida <eiden.yoshida@amd.com>
2021-03-19 01:25:12 -0600

    Allow CI override of machine config

--- .jenkins/precheckin.groovy
+++ .jenkins/precheckin.groovy
@@ -54,7 +54,7 @@ ci: {
     def jobNameList = ["compute-rocm-dkms-no-npi-hipclang":([ubuntu18:['gfx900','gfx906','gfx908']]),
                        "rocm-docker":([ubuntu18:['gfx908']])]
 
-    // jobNameList = auxiliary.appendJobNameList(jobNameList)
+    jobNameList = auxiliary.appendJobNameList(jobNameList)
 
     propertyList.each
     {
