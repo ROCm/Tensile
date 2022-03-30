9ca08f38c4c3bfe6dfa02233637e7e3758c7b6db
zaliu <35415350+zaliu@users.noreply.github.com>
2022-03-30 08:27:32 -0700

    always use hipDeviceAttributePhysicalMultiProcessorCount as multiProcessorCount for Tensile (#1491)

--- Tensile/Source/client/source/HardwareMonitor.cpp
+++ Tensile/Source/client/source/HardwareMonitor.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright 2019-2020 Advanced Micro Devices, Inc.
+ * Copyright 2019-2022 Advanced Micro Devices, Inc.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -64,6 +64,16 @@ namespace Tensile
             hipDeviceProp_t props;
 
             HIP_CHECK_EXC(hipGetDeviceProperties(&props, hipDeviceIndex));
+#if HIP_VERSION >= 50220730
+            int hip_version;
+            HIP_CHECK_EXC(hipRuntimeGetVersion(&hip_version));
+            if(hip_version >= 50220730)
+            {
+                HIP_CHECK_EXC(hipDeviceGetAttribute(&props.multiProcessorCount,
+                                                    hipDeviceAttributePhysicalMultiProcessorCount,
+                                                    hipDeviceIndex));
+            }
+#endif
 
             uint64_t hipPCIID = 0;
             hipPCIID |= props.pciDeviceID & 0xFF;
