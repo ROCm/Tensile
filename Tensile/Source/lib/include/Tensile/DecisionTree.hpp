b9868418b20cc818830fa12b952f07f69815b188
Benjamin Ulmer <benjamin.ulmer@amd.com>
2022-05-06 10:58:58 -0600

    Start integrating decision tree logic with solution library structure

---
+++ Tensile/Source/lib/include/Tensile/DecisionTree.hpp
@@ -0,0 +1,103 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2019-2022 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#pragma once
+
+#include <array>
+#include <functional>
+#include <vector>
+
+namespace Tensile
+{
+    /**
+     * \ingroup SolutionSelection
+     *
+     * A Decision tree model for matching kernels to problem types.
+     * See SolSelDecisionTree_test.cpp for usage and expected behaviour.
+     */
+    namespace DecisionTree
+    {
+        template <typename Key, typename Value, typename ReturnValue>
+        struct Tree
+        {
+            using Transform = std::function<ReturnValue(Value)>;
+
+            struct Node
+            {
+                int          type;
+                float        value;
+                unsigned int nextIdxLeft;
+                unsigned int nextIdxRight;
+            };
+
+            Tree() = default;
+            Tree(std::vector<Key> tree)
+                : tree(std::move(tree))
+            {
+            }
+
+            float predict(Key const& key) const
+            {
+                int  nodeIdx  = 0;
+                int  treeSize = tree.size();
+                Node currentNode;
+
+                while(nodeIdx < treeSize)
+                {
+                    currentNode = tree[nodeIdx];
+                    if(currentNode.type == -1)
+                    { /* End node */
+                        return currentNode.value;
+                    }
+
+                    // Note convention: branch left for less than, else right
+                    if(key[currentNode.type] <= currentNode.value)
+                    {
+                        nodeIdx = currentNode.nextIdxLeft;
+                    }
+                    else
+                    {
+                        nodeIdx = currentNode.nextIdxRight;
+                    }
+                }
+
+                throw std::runtime_error("Decision Tree out of bounds error.");
+                return -1;
+            }
+
+            virtual ReturnValue getSolution(Transform transform) const
+            {
+                return transform(value);
+            }
+
+            bool valid(bool verbose = false) const {}
+
+            std::vector<Node> tree;
+            Value             value;
+        };
+
+    } // namespace DecisionTree
+} // namespace Tensile
---
+++ Tensile/Source/lib/include/Tensile/DecisionTreeLibrary.hpp
@@ -0,0 +1,150 @@
+/*******************************************************************************
+ *
+ * MIT License
+ *
+ * Copyright 2019-2022 Advanced Micro Devices, Inc.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ *
+ *******************************************************************************/
+
+#pragma once
+
+#include <set>
+#include <vector>
+
+#include <Tensile/Debug.hpp>
+#include <Tensile/DecisionTree.hpp>
+#include <Tensile/Properties.hpp>
+#include <Tensile/Utils.hpp>
+
+namespace Tensile
+{
+    /**
+     * \ingroup SolutionLibrary
+     *
+     * TODO: documentation
+     */
+    template <typename Key>
+    struct KeyFactory
+    {
+    };
+
+    template <typename T>
+    struct KeyFactory<std::vector<T>>
+    {
+        static std::vector<T> MakeKey(size_t size)
+        {
+            return std::vector<T>(size);
+        }
+    };
+
+    template <typename T, size_t N>
+    struct KeyFactory<std::array<T, N>>
+    {
+        static std::array<T, N> MakeKey(size_t size)
+        {
+            return std::array<T, N>();
+        }
+    };
+
+    template <typename Key, typename MyProblem, typename MySolution = typename MyProblem::Solution>
+    struct DecisionTreeLibrary : public SolutionLibrary<MyProblem, MySolution>
+    {
+        using Element    = std::shared_ptr<SolutionLibrary<MyProblem, MySolution>>;
+        using Tree       = DecisionTree::Tree<Key, Element, std::shared_ptr<MySolution>>;
+        using Properties = std::vector<std::shared_ptr<Property<MyProblem>>>;
+
+        Properties        properties;
+        std::vector<Tree> trees;
+
+        static std::string Type()
+        {
+            return "DecisionTree";
+        }
+        virtual std::string type() const override
+        {
+            return Type();
+        }
+        virtual std::string description() const override
+        {
+            return "TODO: description";
+        }
+
+        virtual std::shared_ptr<MySolution> findBestSolution(MyProblem const& problem,
+                                                             Hardware const&  hardware,
+                                                             double*          fitness
+                                                             = nullptr) const override
+        {
+            typename Tree::Transform transform
+                = [&](Element library) -> std::shared_ptr<MySolution> {
+                return library->findBestSolution(problem, hardware);
+            };
+
+            Key key = keyForProblem(problem);
+            for(Tree const& tree : trees)
+            {
+                float result = tree.predict(key);
+                if(result > 0)
+                    return tree.getSolution(transform);
+            }
+            return nullptr;
+        }
+
+        virtual SolutionSet<MySolution> findAllSolutions(MyProblem const& problem,
+                                                         Hardware const&  hardware) const override
+        {
+            typename Tree::Transform transform
+                = [&](Element library) -> std::shared_ptr<MySolution> {
+                return library->findBestSolution(problem, hardware);
+            };
+
+            bool debug = Debug::Instance().printPropertyEvaluation();
+
+            SolutionSet<MySolution> rv;
+
+            for(Tree const& tree : trees)
+            {
+                rv.insert(tree.getSolution(transform));
+            }
+
+            return rv;
+        }
+
+        Key keyForProblem(MyProblem const& object) const
+        {
+            bool debug = Debug::Instance().printPropertyEvaluation();
+
+            Key myKey = KeyFactory<Key>::MakeKey(this->properties.size());
+
+            for(int i = 0; i < this->properties.size(); i++)
+                myKey[i] = (*this->properties[i])(object);
+
+            if(debug)
+            {
+                std::cout << "Object key: ";
+                streamJoin(std::cout, myKey, ", ");
+                std::cout << std::endl;
+            }
+
+            return myKey;
+        }
+    };
+
+} // namespace Tensile
