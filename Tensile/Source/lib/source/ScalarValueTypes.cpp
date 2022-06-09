d2bf2e9ed5825a77e365e47adad90bd86dd726ff
Alex Brown <alex.brown@amd.com>
2022-06-08 19:37:54 -0500

    Fix copyright headers

--- Tensile/Source/lib/source/ScalarValueTypes.cpp
+++ Tensile/Source/lib/source/ScalarValueTypes.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright 2021 Advanced Micro Devices, Inc.
+ * Copyright (C) 2021-2022 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
