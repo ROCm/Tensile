19d197e191182545a57af582cf5df29cae55a67c
Alex Brown <alex.brown@amd.com>
2022-11-09 16:40:42 -0700

    Fix replacement kernel name marker Update replacement kernel tests Remove COV2 replacement kernels

--- Tensile/ReplacementKernels.py
+++ Tensile/ReplacementKernels.py
@@ -34,7 +34,7 @@ class ReplacementKernels:
 
     @property
     def marker(self):
-        return '.amdgpu_hsa_kernel'
+        return '.amdhsa_kernel'
 
     def getKernelName(self, filename):
         marker = self.marker
@@ -80,7 +80,7 @@ class ReplacementKernels:
     @classmethod
     def Directory(cls):
         scriptDir = os.path.dirname(os.path.realpath(__file__))
-        dirName = 'ReplacementKernels'
+        dirName = 'ReplacementKernels-cov3'
 
         return os.path.join(scriptDir, dirName)
 
