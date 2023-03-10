86729cae81cf88fbeca14f2ff23ba892ca68f61f
Alex Brown <alex.brown@amd.com>
2023-03-10 13:27:53 -0600

    64-bit offset parameters for post kernels

--- Tensile/Source/lib/source/ContractionSolution.cpp
+++ Tensile/Source/lib/source/ContractionSolution.cpp
@@ -649,6 +649,9 @@ namespace Tensile
         else
             rv.args.append<typename TypedInputs::CType const* const*>("batchC", inputs.batchC);
 
+        rv.args.append<uint64_t>("offsetD", d.offset());
+        rv.args.append<uint64_t>("offsetC", c.offset());
+        
         if(sizeMapping.globalAccumulation)
         {
             size_t stride = d.sizes()[0];
@@ -676,9 +679,6 @@ namespace Tensile
             rv.args.append<uint32_t>(concatenate_if<T_Debug>("size_", idx), size);
             idx++;
         }
-
-        rv.args.append<uint32_t>("offsetD", d.offset());
-        rv.args.append<uint32_t>("offsetC", c.offset());
 
         rv.args.append<typename TypedInputs::BetaType>("beta", inputs.beta);
 
@@ -758,6 +758,9 @@ namespace Tensile
         else
             rv.args.append<typename TypedInputs::CType const* const*>("batchC", inputs.batchC);
 
+        rv.args.append<uint64_t>("offsetD", d.offset());
+        rv.args.append<uint64_t>("offsetC", c.offset());
+
         if(sizeMapping.globalAccumulation == 2)
             rv.args.append<typename TypedInputs::AlphaType>("alpha", inputs.alpha);
         else
@@ -787,9 +790,6 @@ namespace Tensile
             rv.args.append<uint32_t>(concatenate_if<T_Debug>("size_", idx), size);
             idx++;
         }
-
-        rv.args.append<uint32_t>("offsetD", d.offset());
-        rv.args.append<uint32_t>("offsetC", c.offset());
 
         if(sizeMapping.globalAccumulation == 1)
             rv.args.append<uint32_t>("gsu", 1);
