1c0daad81b493b85c147c1f705055e1388c16675
Nielen <niventer@amd.com>
2023-03-13 14:42:13 -0700

    clang-format

--- Tensile/Source/lib/source/ContractionSolution.cpp
+++ Tensile/Source/lib/source/ContractionSolution.cpp
@@ -807,17 +807,18 @@ namespace Tensile
         return (*problemPredicate)(problem) && (*hardwarePredicate)(hardware);
     }
 
-    bool ContractionSolution::matchesProblemType(Problem const& problem, Hardware const& hardware) const
+    bool ContractionSolution::matchesProblemType(Problem const&  problem,
+                                                 Hardware const& hardware) const
     {
         bool matchesHardware = (*hardwarePredicate)(hardware);
         bool matchesType
             = (problemType.aType == problem.a().dataType())
-            && (problemType.bType == problem.b().dataType())
-            && (problemType.cType == problem.c().dataType())
-            && (problemType.dType == problem.d().dataType())
-            && (problemType.highPrecisionAccumulate == problem.highPrecisionAccumulate())
-            && (problemType.stridedBatched == problem.stridedBatched())
-            && (problemType.fp16AltImpl == problem.fp16AltImpl());
+              && (problemType.bType == problem.b().dataType())
+              && (problemType.cType == problem.c().dataType())
+              && (problemType.dType == problem.d().dataType())
+              && (problemType.highPrecisionAccumulate == problem.highPrecisionAccumulate())
+              && (problemType.stridedBatched == problem.stridedBatched())
+              && (problemType.fp16AltImpl == problem.fp16AltImpl());
         return matchesHardware && matchesType;
     }
 
