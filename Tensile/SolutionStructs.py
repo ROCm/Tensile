45aaebe4ce9b8be8d4a432dc179687505e345618
Koji Nakajima <knakajim@amd.com>
2023-04-17 09:13:13 -0600

    Disabled LocalSplitU + I8 for non-MFMA and added MFMA + LocalSplitU + I8 + NN test cases

--- Tensile/SolutionStructs.py
+++ Tensile/SolutionStructs.py
@@ -3065,6 +3065,9 @@ class Solution(collections.abc.Mapping):
         reject(state, "LocalSplitU but MT0*MT1=%u elements doesn't divide into NumThreads=%u" \
             % (state["MacroTile0"]*state["MacroTile1"], state["NumThreads"]))
         return
+      if (not state["EnableMatrixInstruction"]) and state["ProblemType"]["DataType"].isInt8():
+        reject(state, "int8 doesn't support non-MFMA + LocalSplitU")
+        return
 
     # to eliminate identical/duplicate kernels when GSU=1
     if state["GlobalSplitU"] == 1:
