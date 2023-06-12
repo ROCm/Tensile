ROCm/Tensile

commit 9f72124096bd22f5c53395d789adab2a32570bdd

hotfix for fp16alt round near zero mode for rocm 5.6 (#1721)
rocm 5.6 hotfix for fp16alt round near zero mode from develop

Tensile/LibraryIO.py
@@ -181,6 +181,9 @@ def parseLibraryLogicData(data, srcFile="?"):
     if "Fp16AltImpl" not in data:
         data["Fp16AltImpl"] = False
 
+    if "Fp16AltImplRound" not in data:
+        data["Fp16AltImplRound"] = False
+
     if not versionIsCompatible(data["MinimumRequiredVersion"]):
         printWarning("Version = {} in library logic file {} does not match Tensile version = {}" \
                 .format(srcFile, data["MinimumRequiredVersion"], __version__) )
@@ -239,8 +242,11 @@ def parseLibraryLogicList(data, srcFile="?"):
     if len(data) > 10 and data[10]:
         rv["PerfMetric"] = data[10]
 
-    if len(data) > 11 and data[11]:
+    if len(data) > 11 and data[11] == "Fp16AltImpl":
         rv["Fp16AltImpl"] = True
+
+    if len(data) > 12 and data[12] == "Fp16AltImplRound":
+        rv["Fp16AltImplRound"] = True
 
     # library logic fields
     rv["LibraryType"] = "Matching"
@@ -248,8 +254,8 @@ def parseLibraryLogicList(data, srcFile="?"):
     rv["Library"]["indexOrder"] = data[6]
     rv["Library"]["table"] = data[7]
     rv["Library"]["distance"] = "Euclidean"
-    if len(data) > 12 and data[12]:
-        rv["Library"]["distance"] = data[12]
+    if len(data) > 13 and data[13]:
+        rv["Library"]["distance"] = data[13]
 
     return rv
 

Tensile/Common.py
@@ -1058,6 +1058,8 @@ validParameters = {
 
     # alternate implementation for fp16 HPA MFMA
     "Fp16AltImpl": [False, True],
+    # fp16 alternate implementation round mode: false for truncate, true for round near zero
+    "Fp16AltImplRound": [False, True],
 
     # 0  : standard launch
     # N>0 : launch persistent kernel with N workgroups per compute unit
@@ -1433,6 +1435,7 @@ defaultBenchmarkCommonParameters = [
     {"StoreCInUnrollExact":       [ False ] },
     {"StoreCInUnrollPostLoop":    [ False ] },
     {"Fp16AltImpl":               [ False ] },
+    {"Fp16AltImplRound":          [ False ] },
     {"ThreadSeparateGlobalReadA": [ 0 ] },
     {"ThreadSeparateGlobalReadB": [ 0 ] }
     ]
@@ -1638,7 +1641,8 @@ defaultProblemType = {
     "TileAwareSelection":       False,
 
     # FP16 Alternate Implementation
-    "Fp16AltImpl":              False
+    "Fp16AltImpl":              False,
+    "Fp16AltImplRound":         False
     }
 
 defaultProblemSizes = [{"Range": [ [2880], 0, 0 ]}]

Tensile/Contractions.py
@@ -340,6 +340,9 @@ class ProblemPredicate(Properties.Predicate):
         if key == "Fp16AltImpl":
             return cls("Fp16AltImpl") if value != False else None
 
+        if key == "Fp16AltImplRound":
+            return cls("Fp16AltImplRound") if value != False else None
+
     @classmethod
     def CompoundPredicates(cls, state, problemType):
         rv = []

Tensile/KernelWriterAssembly.py
@@ -625,6 +625,11 @@ class KernelWriterAssembly(KernelWriter):
       numberOfSgpr = self.numGlobalReadOffsetsB if needFirstSgprOffset else (self.numGlobalReadOffsetsB-1)
       self.defineSgpr("ScalarGlobalReadOffsetB", numberOfSgpr)
 
+    # Register to store rounding term if fp16 alt rnz
+    if kernel["ProblemType"]["Fp16AltImpl"] and kernel["ProblemType"]["Fp16AltImplRound"]:
+      self.defineSgpr("Fp16AltOffset", 1)
+      self.defineSgpr("Fp16AltNanCheck", 2, 2)
+
     # debug flag to allocate dummy / unused sgpr
     # useful when comparing code that adds new kernel arguments to see what
     # was actually changed
@@ -1320,6 +1325,8 @@ class KernelWriterAssembly(KernelWriter):
 
     if kernel["ProblemType"]["Fp16AltImpl"]:
       self.numG2LpipeRegisters = 2
+      if kernel["ProblemType"]["Fp16AltImplRound"]:
+        self.numG2LpipeRegisters += 1
 
     ####################################
     # num vgprs: c write address
@@ -1508,6 +1515,9 @@ class KernelWriterAssembly(KernelWriter):
       self.G2Lpipe0 = vgprIdx
       self.G2Lpipe1 = self.G2Lpipe0 + 1
       vgprIdx += 2
+      if kernel["ProblemType"]["Fp16AltImplRound"]:
+        self.Fp16AltTmp = self.G2Lpipe1 + 1
+        vgprIdx += 1
 
     self.startVgprAddressDbg = vgprIdx
     vgprIdx += numVgprAddressDbg
@@ -2492,6 +2502,8 @@ class KernelWriterAssembly(KernelWriter):
     if kernel["ProblemType"]["Fp16AltImpl"]:
       kStr += self.macroRegister("vgprG2Lpipe0", self.G2Lpipe0)
       kStr += self.macroRegister("vgprG2Lpipe1", self.G2Lpipe1)
+      if kernel["ProblemType"]["Fp16AltImplRound"]:
+        kStr += self.macroRegister("vgprFp16AltTmp", self.Fp16AltTmp)
 
     # Serial is always the last register in the pool so the store
     # code doesn't have to deal with fragmentation
@@ -3224,6 +3236,10 @@ class KernelWriterAssembly(KernelWriter):
       for idxChar in sorted(set(kernel["PackedC0IdxChars"][:-1] + kernel["PackedC1IdxChars"][:-1])):
           kStr += inst("s_lshr_b32", sgpr("MagicAbitSize%s"%idxChar), sgpr("MagicShiftSize%s"%idxChar), 31,"extract abit")
           kStr += inst("s_and_b32",  sgpr("MagicShiftSize%s"%idxChar), sgpr("MagicShiftSize%s"%idxChar), hex(0x7fffffff), "remove abit")
+
+    # setup rounding term if Fp16AltImpl is enabled, reusing sgpr to hold rounding term
+    if kernel["ProblemType"]["Fp16AltImpl"] and kernel["ProblemType"]["Fp16AltImplRound"]:
+      kStr += inst("s_mov_b32", sgpr("Fp16AltOffset"), "0x7FFF", "Set rounding coeff if Fp16AltImplNtz enabled")
 
     ########################################
     # Debug Buffer
@@ -7167,7 +7183,7 @@ class KernelWriterAssembly(KernelWriter):
         # add a wrap increment, if needed:
         incLower = self.getTmpSgpr(3).idx()
         incUpper = incLower + 1
-        tmpS =    incLower + 2
+        tmpS     = incLower + 2
         if prefetchIndex:
           imod.addInst("s_add_u32", sgpr(tmpS), self.loopCounter(kernel, self.unrollIdx), prefetchIndex, "remove pf(%u)"%prefetchIndex)
           imod.addInst("s_cmp_eq_u32",  sgpr("StaggerUIter"), sgpr(tmpS), "Is this wrapIter? (pf)")
@@ -8677,6 +8693,11 @@ class KernelWriterAssembly(KernelWriter):
 
       tmpLocalWriteAddr = -1
 
+      # due to bug in scheduler, cannot use temp sgpr for now
+      #tempFp16Sgpr = -1
+      #if kernel["ProblemType"]["Fp16AltImpl"] and kernel["ProblemType"]["Fp16AltImplRound"]:
+      #  tempFp16Sgpr = self.sgprPool.checkOutAligned(self.laneSGPRCount, 2, "FP16 Alt Round Temp SGPRs")
+
       # using _ds_store_b8: need one more vgpr space to do lshr
       tmpVgprOffset = ((self.numVgprG2LA if (tP['tensorChar'] == 'A') else self.numVgprG2LB) / 2) if (blockWidth == 0.25) else 0
 
@@ -8765,14 +8786,23 @@ class KernelWriterAssembly(KernelWriter):
               if self.db["ForceInputValue%s"%tc]:
                 localWriteCode.addInst("v_mov_b32", vgpr("G2L%s+%u"%(tc, g2lIdx)), self.db["ForceValue%s"%tc], "ForceInputValue")
               if kernel["ProblemType"]["Fp16AltImpl"]:
-                numIters = 1 if blockWidth <= 1 else blockWidth 
+                numIters = 1 if blockWidth <= 1 else blockWidth
                 for iter in range(0, numIters):
-                   vgprsrc = vgpr("G2L%s+%u"%(tc, g2lIdx+iter))
-                   vgprsrc += " src0_sel:WORD_1"
-                   localWriteCode.addInst("v_cvt_f32_f16", vgpr("G2Lpipe0"), vgpr("G2L%s+%u"%(tc, g2lIdx+iter)),"")
-                   localWriteCode.addInst("v_cvt_f32_f16", vgpr("G2Lpipe1"), vgprsrc,"")
-                   localWriteCode.addInst("v_pack_b32_f16", vgpr("G2L%s+%u"%(tc, g2lIdx+iter)), vgpr("G2Lpipe0"),vgpr("G2Lpipe1"), "op_sel:[1,1,0]","")
+                  vgprsrc = vgpr("G2L%s+%u"%(tc, g2lIdx+iter))
 
+                  localWriteCode.addInst("v_cvt_f32_f16", vgpr("G2Lpipe0"), vgprsrc,"")
+                  if kernel["ProblemType"]["Fp16AltImplRound"]:
+                    localWriteCode.addInst("v_cmp_u_f32", sgpr("Fp16AltNanCheck", self.laneSGPRCount), vgpr("G2Lpipe0"), vgpr("G2Lpipe0"), "check NaN" )
+                    localWriteCode.addInst("v_add_u32", vgpr("Fp16AltTmp"), sgpr("Fp16AltOffset"), vgpr("G2Lpipe0"), "")
+                    localWriteCode.addInst("v_cndmask_b32", vgpr("G2Lpipe0"), vgpr("Fp16AltTmp"), vgpr("G2Lpipe0"), sgpr("Fp16AltNanCheck",self.laneSGPRCount), "" )
+
+                  localWriteCode.addInst("v_cvt_f32_f16", vgpr("G2Lpipe1"), vgprsrc, "src0_sel:WORD_1", "")
+                  if kernel["ProblemType"]["Fp16AltImplRound"]:
+                    localWriteCode.addInst("v_cmp_u_f32", sgpr("Fp16AltNanCheck",self.laneSGPRCount), vgpr("G2Lpipe1"), vgpr("G2Lpipe1"), "check NaN" )
+                    localWriteCode.addInst("v_add_u32", vgpr("Fp16AltTmp"), sgpr("Fp16AltOffset"), vgpr("G2Lpipe1"), "")
+                    localWriteCode.addInst("v_cndmask_b32", vgpr("G2Lpipe1"), vgpr("Fp16AltTmp"), vgpr("G2Lpipe1"), sgpr("Fp16AltNanCheck",self.laneSGPRCount), "" )
+
+                  localWriteCode.addInst("v_pack_b32_f16", vgprsrc, vgpr("G2Lpipe0"),vgpr("G2Lpipe1"), "op_sel:[1,1,0]","")
 
             for oIdx in range(0, numOffsets):
               paramList.append(offset)
@@ -8808,6 +8838,10 @@ class KernelWriterAssembly(KernelWriter):
             loopCnt+=1
       if tmpLocalWriteAddr != -1:
         self.vgprPool.checkIn(tmpLocalWriteAddr)
+
+      # due to bug in scheduler, cannot use temp sgpr for now
+      #if tempFp16Sgpr != -1:
+      #  self.sgprPool.checkIn(tempFp16Sgpr)
 
     # localWriteDoCnt<=2 is prefetch if PrefetchGlobalRead:
     if 0 and tP["isB"]: # post-lds-write

Tensile/SolutionLibrary.py
@@ -324,8 +324,21 @@ class MasterSolutionLibrary:
             newLib = PredicateLibrary(tag="Problem")
             newLib.rows.append({"predicate": predicate, "library": library})
 
-            if lazyLibrary and predicate.tag != "TruePred":
+            if lazyLibrary and predicate.tag == "Fp16AltImpl" and not d.get("Fp16AltImplRnz"):
                 placeholderName += "_Fp16Alt"
+
+            return newLib, placeholderName
+
+        def fp16AltImplRound(d, problemType, solutions, library, placeholderName):
+            if d.get("Fp16AltImplRound"):
+                predicate = Properties.Predicate(tag="Fp16AltImplRound")
+            else:
+                predicate = Properties.Predicate(tag="TruePred")
+            newLib = PredicateLibrary(tag="Problem")
+            newLib.rows.append({"predicate": predicate, "library": library})
+
+            if lazyLibrary and predicate.tag == "Fp16AltImplRound":
+                placeholderName += "_Fp16AltRnz"
 
             return newLib, placeholderName
 
@@ -377,12 +390,12 @@ class MasterSolutionLibrary:
         if libraryOrder is None:
             if Common.globalParameters["LazyLibraryLoading"]:
                 libraryOrder = [
-                    hardware, operationIdentifier, performanceMetric, fp16AltImpl, predicates,
+                    hardware, operationIdentifier, performanceMetric, fp16AltImpl, fp16AltImplRound, predicates,
                     placeholder, selection
                 ]
             else:
                 libraryOrder = [
-                    hardware, operationIdentifier, performanceMetric, fp16AltImpl, predicates,
+                    hardware, operationIdentifier, performanceMetric, fp16AltImpl, fp16AltImplRound, predicates,
                     selection
                 ]
         #assert libraryOrder[-1] == selection

Tensile/SolutionStructs.py
@@ -1141,7 +1141,9 @@ class ProblemType(Mapping):
     # Other
     if self["UseBeta"]: name += "B"
     if self["HighPrecisionAccumulate"] and not self["SilentHighPrecisionAccumulate"]: name += "H"
-    if self["Fp16AltImpl"]: name += "R"
+    if self["Fp16AltImpl"]:
+      if self["Fp16AltImplRound"]: name += "RZ"
+      else: name += "R"
     if self["UseInitialStridesAB"]: name += "I"
     if self["UseInitialStridesCD"]: name += "Ic"
 
@@ -4333,6 +4335,7 @@ class Solution(collections.abc.Mapping):
     requiredParameters["MatrixInstBN"]      = False # always prepended
     requiredParameters["CustomKernelName"]  = False # Will not affect naming
     requiredParameters["Fp16AltImpl"]       = False # Will show up as a different type
+    requiredParameters["Fp16AltImplRound"]  = False # Will show up as a different type
 
     requiredParameters["Kernel"]            = True  # distinguish kernels from solutions
                                                     # for single-source compilation

Tensile/Source/client/include/ClientProblemFactory.hpp
@@ -84,6 +84,7 @@ namespace Tensile
             KernelLanguage    m_kernelLanguage;
             PerformanceMetric m_performanceMetric;
             bool              m_fp16AltImpl;
+            bool              m_fp16AltImplRound;
 
             std::vector<std::vector<size_t>> m_problemSizes;
             std::vector<std::vector<size_t>> m_aStrides;

Tensile/Source/client/source/ClientProblemFactory.cpp
@@ -203,6 +203,7 @@ namespace Tensile
                 rv.back().setDeterministicMode(m_deterministicMode);
                 rv.back().setArithmeticUnit(m_arithmeticUnit);
                 rv.back().setFp16AltImpl(m_fp16AltImpl);
+                rv.back().setFp16AltImplRound(m_fp16AltImplRound);
 
                 if(m_convProblemSizes.size())
                     rv.back().setConvProblemSizes(m_convProblemSizes[i]);

commit 7d0a9d040c3bbae893df7ecef6a19d9cd1c304aa

fixing predicate ordering for fp16alt impl to unbreak distance modes (#1729)
fixing bug introduced by PR #1721 into 5.6.

Tensile/LibraryIO.py
@@ -245,7 +245,7 @@ def parseLibraryLogicList(data, srcFile="?"):
     if len(data) > 11 and data[11] == "Fp16AltImpl":
         rv["Fp16AltImpl"] = True
 
-    if len(data) > 12 and data[12] == "Fp16AltImplRound":
+    if len(data) > 13 and data[13] == "Fp16AltImplRound":
         rv["Fp16AltImplRound"] = True
 
     # library logic fields
@@ -254,8 +254,8 @@ def parseLibraryLogicList(data, srcFile="?"):
     rv["Library"]["indexOrder"] = data[6]
     rv["Library"]["table"] = data[7]
     rv["Library"]["distance"] = "Euclidean"
-    if len(data) > 13 and data[13]:
-        rv["Library"]["distance"] = data[13]
+    if len(data) > 12 and data[12]:
+        rv["Library"]["distance"] = data[12]
 
     return rv
 