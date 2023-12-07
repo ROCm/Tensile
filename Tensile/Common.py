1cc59fc0eeea363c86b160bd610d1ebd03bd7130
Scott Quiring <scott.quiring@amd.com>
2023-12-07 22:32:47 +0000

    Revert globalParameter change.

--- Tensile/Common.py
+++ Tensile/Common.py
@@ -265,7 +265,7 @@ globalParameters["SeparateArchitectures"] = False # write Tensile library metada
 
 globalParameters["LazyLibraryLoading"] = False # Load library and code object files when needed instead of at startup
 
-globalParameters["IgnoreAsmCapCache"] = True # Ignore checking for discrepancies between derived and cached asm caps
+globalParameters["IgnoreAsmCapCache"] = False # Ignore checking for discrepancies between derived and cached asm caps
 
 globalParameters["ExperimentalLogicDir"] = "/experimental/"
 
