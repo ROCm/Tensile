3c85499d28cde0d87c71f5cd3a6d221340484f29
David Dixon <david.dixon@amd.com>
2024-06-19 16:09:54 +0000

    Add conditional default

--- Tensile/TensileCreateLibrary.py
+++ Tensile/TensileCreateLibrary.py
@@ -1121,7 +1121,8 @@ def TensileCreateLibrary():
   argParser.add_argument("LogicPath",       help="Path to LibraryLogic.yaml files.")
   argParser.add_argument("OutputPath",      help="Where to write library files?")
   argParser.add_argument("RuntimeLanguage", help="Which runtime language?", choices=["OCL", "HIP", "HSA"])
-  argParser.add_argument("--cxx-compiler",           dest="CxxCompiler",       choices=['amdclang++', "clang++", "hipcc"],       action="store", default="amdclang++")
+  argParser.add_argument("--cxx-compiler",           dest="CxxCompiler",       choices=['amdclang++', "clang++", "hipcc"], action="store", 
+                         default="amdclang++" if os.name != "nt" else "clang++")
   argParser.add_argument("--cmake-cxx-compiler",     dest="CmakeCxxCompiler",  action="store")
   argParser.add_argument("--code-object-version",    dest="CodeObjectVersion", choices=["default", "V4", "V5"], action="store")
   argParser.add_argument("--architecture",           dest="Architecture",      type=str, action="store", default="all", 
