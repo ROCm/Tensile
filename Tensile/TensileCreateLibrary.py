adba4aefacc06cbbc29a8eeca5ee71f3a4c87a0a
David Dixon <david.dixon@amd.com>
2024-06-20 18:22:07 +0000

    Corrects missing Path constructor

--- Tensile/TensileCreateLibrary.py
+++ Tensile/TensileCreateLibrary.py
@@ -1320,11 +1320,6 @@ def TensileCreateLibrary():
   for logicFile in logicFiles:
     print1("#   %s" % logicFile)
 
-  ##############################################################################
-  # Parse config files
-  ##############################################################################
-
-  # Parse logicData, solutions, and masterLibraries from logic files
   solutions, masterLibraries, fullMasterLibrary = generateLogicDataAndSolutions(logicFiles, args)
 
   if globalParameters["LazyLibraryLoading"] and arguments["WriteMasterSolutionIndex"]:
@@ -1338,7 +1333,6 @@ def TensileCreateLibrary():
 
   staticFiles = copyStaticFiles(outputPath)
 
-  # Build a list of files to be expected
   (solutionFiles,
    sourceKernelFiles,
    asmKernelFiles,
@@ -1357,7 +1351,6 @@ def TensileCreateLibrary():
   toFile(Path(manifestFile), libMetadataPaths + sourceLibPaths + asmLibPaths)
   if globalParameters["GenerateManifestAndExit"]: return
 
-  # generate cmake for the source kernels,
   if not arguments["GenerateSourcesAndExit"]:
     writeCMake(outputPath, solutionFiles, sourceKernelFiles, staticFiles, masterLibraries)
 
@@ -1366,10 +1359,8 @@ def TensileCreateLibrary():
     shutil.copy( os.path.join(globalParameters["SourcePath"], fileName), \
       outputPath )
 
-  # write solutions and kernels
   codeObjectFiles = writeKernels(outputPath, CxxCompiler, None, solutions,
                                              kernels, kernelHelperObjs, kernelWriterSource, kernelWriterAssembly)
-
     
   sanityCheck(sourceLibPaths, asmLibPaths, codeObjectFiles, globalParameters["GenerateSourcesAndExit"])
 
@@ -1415,8 +1406,7 @@ def TensileCreateLibrary():
   
   ext = ".yaml" if globalParameters["LibraryFormat"] == "yaml" else ".dat"
   if args.EmbedLibrary:
-    embedFileName = outputPath / f"library/{args.EmbedLibrary}"
-    ext = ".yaml" if globalParameters["LibraryFormat"] == "yaml" else ".dat"
+    embedFileName = Path(outputPath) / f"library/{args.EmbedLibrary}"
     EmbeddedData.generateLibrary(embedFileName, args.EmbedLibraryKey, Path(masterFile).with_suffix(ext), theMasterLibrary.cpp_base_class, codeObjectFiles)
 
   if args.BuildClient:
