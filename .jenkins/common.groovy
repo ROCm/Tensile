f12d043994b8c5ea2663cb45fefcbd92ea375272
David Dixon <david.dixon@amd.com>
2024-06-27 11:29:50 +0000

    Only archive timing if not rhel9

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -117,7 +117,8 @@ def runTestCommand(platform, project, jobName, testMark, boolean runHostTest=tru
         """
     platform.runCommand(this, command)
 
-    archiveArtifacts "${project.paths.project_build_prefix}/timing*.csv"
+    if (platform.os != "rhel9")        
+        archiveArtifacts "${project.paths.project_build_prefix}/timing*.csv"
     if (runUnitTest) {
         recordCoverage(tools: [[parser: 'COBERTURA', pattern: "${project.paths.project_build_prefix}/cobertura.xml"]])
     }
