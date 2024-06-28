e4d78b5b58c6d40cba580ae2b34191a347f40d78
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2024-06-28 15:44:32 +0000

    ci: add debug commands

--- .jenkins/common.groovy
+++ .jenkins/common.groovy
@@ -55,7 +55,9 @@ def runCompileCommand(platform, project, jobName, boolean debug=false)
             export TENSILE_COMPILER=${compiler}
             export HIPCC_COMPILE_FLAGS_APPEND='-O3 -Wno-format-nonliteral -parallel-jobs=4'
 
-            pip3 install -r requirements.txt
+            pwd
+            cat requirements.txt
+            pip3 install -r `pwd`/requirements.txt
 
             mkdir build && pushd build
 
