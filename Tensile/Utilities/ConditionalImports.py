43bf05f693d116b77166512f7b72b88b17c96200
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2024-07-15 21:14:06 +0000

    chore: ignore flake8 for conditional imports

--- Tensile/Utilities/ConditionalImports.py
+++ Tensile/Utilities/ConditionalImports.py
@@ -10,11 +10,9 @@ except ImportError:
 try:
     from yaml import CSafeLoader as yamlLoader
 except ImportError:
-    warnings.warn("Couldn't import CSafeLoader backend, defaulting to slower SafeDumper")
     from yaml import SafeLoader as yamlLoader
 
 try:
     from yaml import CSafeDumper as yamlDumper 
 except ImportError:
-    warnings.warn("Couldn't import CSafeDumper backend, defaulting to slower SafeDumper")
     from yaml import SafeDumper as yamlDumper
