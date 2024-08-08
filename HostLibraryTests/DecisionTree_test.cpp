0aea5a6ddce74732303451bf8a454df998b7abfe
David Dixon <david.dixon@amd.com>
2024-08-08 18:33:59 +0000

    Fix data range

--- HostLibraryTests/DecisionTree_test.cpp
+++ HostLibraryTests/DecisionTree_test.cpp
@@ -2,7 +2,7 @@
  *
  * MIT License
  *
- * Copyright (C) 2024 Advanced Micro Devices, Inc. All rights reserved.
+ * Copyright (C) 2022-2024 Advanced Micro Devices, Inc. All rights reserved.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
