271b533313d9854738d90f2b9943c5516e685160
David Dixon <david.dixon@amd.com>
2024-08-13 23:21:56 +0000

    Removing intermediate object files breaks gfx90a builds

--- Tensile/TensileCreateLibrary.py
+++ Tensile/TensileCreateLibrary.py
@@ -166,9 +166,9 @@ def getAssemblyCodeObjectFiles(kernels, kernelWriterAssembly, outputPath, remove
                     print(err.output)
                     raise
 
-                if removeTemporaries:
-                    for objectFile in objectFiles:
-                        os.remove(os.path.join(asmDir, objectFile))
+                #if removeTemporaries:
+                   #for objectFile in objectFiles:
+                   #     os.remove(os.path.join(asmDir, objectFile))
 
                 coFiles.append(coFile)
         else:
