d7ef1f5d95ba5ba009bc4db86e2c47901dd1c523
amd-garydeng <garydeng@amd.com>
2024-10-02 11:43:03 -0600

    Adding gfx1151 to 6.2 (#2026)
    * add gfx1151 to 6.2
    * skip unsupported tests
    
    (cherry picked from commit 81ae9537671627fe541332c0a5d953bfd6af71d6)

--- Tensile/Common.py
+++ Tensile/Common.py
@@ -254,7 +254,8 @@ globalParameters["SupportedISA"] = [(8,0,3),
                                     (9,4,0), (9,4,1), (9,4,2),
                                     (10,1,0), (10,1,1), (10,1,2), (10,3,0), (10,3,1),
                                     (11,0,0), (11,0,1), (11,0,2),
-                                    (12,0,0), (12,0,1)] # assembly kernels writer supports these architectures
+                                    (12,0,0), (12,0,1), # assembly kernels writer supports these architectures
+                                    (11,5,1)] # assembly kernels writer supports these architectures
 
 globalParameters["CleanupBuildFiles"] = False                     # cleanup build files (e.g. kernel assembly) once no longer needed
 globalParameters["GenerateManifestAndExit"] = False               # Output manifest file with list of expected library objects and exit
@@ -335,7 +336,8 @@ architectureMap = {
   'gfx1030':'navi21', 'gfx1031':'navi22', 'gfx1032':'navi23', 'gfx1034':'navi24', 'gfx1035':'rembrandt',
   'gfx1100':'navi31', 'gfx1101':'navi32', 'gfx1102':'navi33',
   'gfx1200':'gfx1200',
-  'gfx1201':'gfx1201'
+  'gfx1201':'gfx1201',
+  'gfx1151':'gfx1151'
 }
 
 def getArchitectureName(gfxName: str) -> Optional[str]:
