e3004bcf33c8c988e5396d3d0ad6cae6785dc779
Stella Laurenzo <stellaraccident@gmail.com>
2025-05-26 10:24:00 -0400

    Limit number of Tensile workers to 61 on Windows due to system limit.

--- Tensile/Parallel.py
+++ Tensile/Parallel.py
@@ -36,7 +36,10 @@ def CPUThreadCount(enable=True):
         return 1
     else:
         if os.name == "nt":
-            cpu_count = os.cpu_count()
+            # Windows supports at most 61 workers because the scheduler uses
+            # WaitForMultipleObjects directly, which has the limit (the limit
+            # is actually 64, but some handles are needed for accounting).
+            cpu_count = min(os.cpu_count(), 61)
         else:
             cpu_count = len(os.sched_getaffinity(0))
         cpuThreads = globalParameters["CpuThreads"]
