b143f40e5988a2411165f02343178f698760ca1f
Braden Stefanuk <121893577+bstefanuk@users.noreply.github.com>
2025-06-12 19:42:18 +0000

    fix: add copyright headers

--- Tensile/BuildCommands/AssemblyCommands.py
+++ Tensile/BuildCommands/AssemblyCommands.py
@@ -1,3 +1,6 @@
+# Copyright Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier: MIT
+
 import collections
 import os
 import shutil
--- Tensile/BuildCommands/SharedCommands.py
+++ Tensile/BuildCommands/SharedCommands.py
@@ -1,3 +1,6 @@
+# Copyright Advanced Micro Devices, Inc., or its affiliates.
+# SPDX-License-Identifier: MIT
+
 import os
 import subprocess
 from pathlib import Path
